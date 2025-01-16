7674e2c8a7dea8799bb577a6661a3b1cf59f6c5a
joowojr <joowojr@gmail.com>
2025-01-17 08:24:35 +0900

    CLAP-28 :sparkles: Feat: Task 도메인에 대한 entity 추가
    <footer>
    - 관련: #2

---
+++ src/main/java/clap/server/adapter/out/persistense/entity/task/AttachmentEntity.java
@@ -0,0 +1,38 @@
+package clap.server.adapter.out.persistense.entity.task;
+
+import clap.server.adapter.out.persistense.entity.common.BaseTimeEntity;
+import clap.server.adapter.out.persistense.entity.history.CommentEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "attachment")
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AttachmentEntity extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "attachment_id", nullable = false)
+    private Long attachmentId;
+
+    @Column(nullable = false)
+    private String originalName;
+
+    @Column(nullable = false)
+    private String fileUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id")
+    private TaskEntity task;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private CommentEntity comment;
+
+    @Column(nullable = false)
+    private String fileSize;
+}
---
+++ src/main/java/clap/server/adapter/out/persistense/entity/task/constant/LabelStatus.java
@@ -0,0 +1,4 @@
+package clap.server.adapter.out.persistense.entity.task.constant;
+
+public class LabelStatus {
+}
