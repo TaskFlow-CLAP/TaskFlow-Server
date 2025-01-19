966d1f752853331457b0917e0825cbdfadd146d3
joowojr <joowojr@gmail.com>
2025-01-19 15:29:09 +0900

    CLAP-67 Refactor: 애플리케이션/ 도메인 레이어 exception 분리
    <footer>
    - 관련: #26

---
+++ src/main/java/clap/server/exception/BaseException.java
@@ -0,0 +1,24 @@
+package clap.server.exception;
+
+import clap.server.exception.code.BaseErrorCode;
+import lombok.Getter;
+
+
+@Getter
+public abstract class BaseException extends RuntimeException {
+
+    private final BaseErrorCode code;
+
+    protected BaseException(BaseErrorCode code) {
+        super(code.getMessage());
+        this.code = code;
+    }
+
+    public static <T extends BaseException> T from(BaseErrorCode code, Class<T> exceptionClass) {
+        try {
+            return exceptionClass.getConstructor(BaseErrorCode.class).newInstance(code);
+        } catch (Exception e) {
+            throw new RuntimeException("Could not create exception instance", e);
+        }
+    }
+}
