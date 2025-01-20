56f97449ec14613096057587fe80d25ae9d4097d
joowojr <joowojr@gmail.com>
2025-01-20 12:27:08 +0900

    CLAP-90 Bug: 엔티티 변경 사항 모델 클래스에 적용
    <footer>
    - 관련: #43

--- src/main/java/clap/server/domain/model/task/Status.java
+++ src/main/java/clap/server/domain/model/task/Status.java
@@ -1,6 +1,8 @@
 package clap.server.domain.model.task;
 
+import clap.server.adapter.outbound.persistense.entity.member.MemberEntity;
 import clap.server.adapter.outbound.persistense.entity.task.constant.StatusFlag;
+import clap.server.adapter.outbound.persistense.entity.task.constant.TaskStatus;
 import clap.server.domain.model.common.BaseTime;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -12,9 +14,8 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Status extends BaseTime {
     private Long statusId;
-    private Long adminId;
-    private String code;
-    private String name;
-    public StatusFlag statusFlag;
-
+    private MemberEntity admin;
+    private TaskStatus statusName;
+    private StatusFlag statusFlag;
+    private boolean isDeleted;
  }
