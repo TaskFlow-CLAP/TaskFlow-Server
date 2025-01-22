c7887adbdb1c8eed7e6257a1ab549e4669959bc2
joowojr <joowojr@gmail.com>
2025-01-23 00:50:55 +0900

    CLAP-57 Feat: 초기 비밀번호 생성 util 및 관련 설정 추가
    <footer>
    - 관련: #14

---
+++ src/main/java/clap/server/common/utils/InitialPasswordGenerator.java
@@ -0,0 +1,37 @@
+package clap.server.common.utils;
+
+import clap.server.common.properties.PasswordPolicyProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+
+@Component
+@RequiredArgsConstructor
+public class InitialPasswordGenerator {
+
+    private final PasswordPolicyProperties properties;
+
+    public String generateRandomPassword() {
+        return generateRandomPassword(properties.getLength());
+    }
+
+    public String generateRandomPassword(int length) {
+        if (length <= 0) {
+            throw new IllegalArgumentException("Password length must be greater than 0");
+        }
+
+        SecureRandom secureRandom = new SecureRandom();
+        StringBuilder password = new StringBuilder(length);
+
+        String characters = properties.getCharacters();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(properties.getLength());
+            password.append(characters.charAt(randomIndex));
+        }
+
+        return password.toString();
+    }
+}
