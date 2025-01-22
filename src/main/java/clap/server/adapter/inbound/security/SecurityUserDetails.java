8f435b0b3c283ed5da140cf63a867153bc2b0acb
joowojr <joowojr@gmail.com>
2025-01-22 23:07:42 +0900

    CLAP-84 Feat: 사용자 정보를 처리하는 SecurityUserDetailsService 및 SecurityUserDetails 구현
    <footer>
    - 관련: #42

---
+++ src/main/java/clap/server/adapter/inbound/security/SecurityUserDetails.java
@@ -0,0 +1,93 @@
+package clap.server.adapter.inbound.security;
+
+import clap.server.adapter.outbound.persistense.entity.member.MemberEntity;
+import clap.server.adapter.outbound.persistense.entity.member.constant.MemberStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serial;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SecurityUserDetails implements UserDetails {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long userId;
+    private String username;
+    private Collection<? extends GrantedAuthority> authorities;
+    private boolean accountNonLocked;
+
+    @JsonIgnore
+    private boolean enabled;
+    @JsonIgnore
+    private String password;
+    @JsonIgnore
+    private boolean credentialsNonExpired;
+    @JsonIgnore
+    private boolean accountNonExpired;
+
+    @Builder
+    public SecurityUserDetails(
+            Long userId,
+            String username,
+            Collection<? extends GrantedAuthority> authorities,
+            boolean accountNonLocked
+    ) {
+        this.userId = userId;
+        this.username = username;
+        this.authorities = authorities;
+        this.accountNonLocked = accountNonLocked;
+    }
+
+    public static UserDetails from(MemberEntity member) {
+        return SecurityUserDetails.builder()
+                .userId(member.getMemberId())
+                .username(member.getName())
+                .authorities(List.of(new CustomGrantedAuthority(member.getRole().name())))
+                .accountNonLocked(member.getStatus().equals(MemberStatus.INACTIVE))
+                .build();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        throw new UnsupportedOperationException();
+    }
+}
