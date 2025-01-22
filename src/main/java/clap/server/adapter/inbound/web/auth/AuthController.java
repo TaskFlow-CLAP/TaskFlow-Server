2092e3caf42bf141fda909ad04c3a8d2124eef5d
joowojr <joowojr@gmail.com>
2025-01-23 00:28:47 +0900

    CLAP-84 Docs: swagger 명세 추가
    <footer>
    - 관련: #43

--- src/main/java/clap/server/adapter/inbound/web/auth/AuthController.java
+++ src/main/java/clap/server/adapter/inbound/web/auth/AuthController.java
@@ -5,17 +5,17 @@ import clap.server.adapter.inbound.web.dto.auth.LoginResponse;
 import clap.server.application.port.inbound.auth.AuthUsecase;
 import clap.server.common.annotation.architecture.WebAdapter;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Tag(name = "로그인 / 로그아웃 / 토큰 재발급")
 @WebAdapter
 @RequiredArgsConstructor
 @RequestMapping("/api/auths")
-@Slf4j
 public class AuthController {
     private final AuthUsecase authUsecase;
 
--- src/main/java/clap/server/adapter/inbound/web/dto/admin/RegisterMemberRequest.java
+++ src/main/java/clap/server/adapter/inbound/web/dto/admin/RegisterMemberRequest.java
@@ -1,23 +1,25 @@
 package clap.server.adapter.inbound.web.dto.admin;
 
 import clap.server.adapter.outbound.persistense.entity.member.constant.MemberRole;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record RegisterMemberRequest(
-        @NotBlank
+        @NotBlank @Schema(description = "회원 이름")
         String name,
-        @NotBlank
+        @NotBlank @Schema(description = "회원 이메일")
         String email,
-        @NotBlank
+        @NotBlank @Schema(description = "회원 닉네임, 로그인할 때 쓰입니다.") @Pattern(regexp = "^[a-z]{3,10}\\.[a-z]{1,5}$", message = "올바른 형식이 아닙니다.")
         String nickname,
-        @NotNull
+        @NotNull @Schema(description = "승인 권한 여부")
         Boolean isReviewer,
-        @NotNull
+        @NotNull @Schema(description = "부서 ID")
         Long departmentId,
-        @NotNull
+        @NotNull @Schema(description = "회원 역할")
         MemberRole role,
-        @NotBlank
+        @NotBlank @Schema(description = "회원 직책")
         String departmentRole
 ) {
 }
