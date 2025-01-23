d7590f3de2dbe56dfb1ee6ed2678b3e020a62b2a
joowojr <joowojr@gmail.com>
2025-01-23 13:30:46 +0900

    CLAP-61 Feat: 비밀번호 형식 validation 추가
    - 관련: #20

--- src/main/java/clap/server/adapter/inbound/web/member/ResetPasswordController.java
+++ src/main/java/clap/server/adapter/inbound/web/member/ResetPasswordController.java
@@ -3,9 +3,11 @@ package clap.server.adapter.inbound.web.member;
 import clap.server.adapter.inbound.security.SecurityUserDetails;
 import clap.server.application.port.inbound.auth.ResetPasswordUsecase;
 import clap.server.common.annotation.architecture.WebAdapter;
+import clap.server.common.annotation.validation.password.ValidPassword;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -23,14 +25,14 @@ public class ResetPasswordController {
     @Operation(summary = "초기 로그인 후 비밀번호 재설정 API", description = "swagger에서 따옴표를 포함하지 않고 요청합니다.")
     @PatchMapping("/members/initial-password")
     public void resetPasswordAndActivateMember(@AuthenticationPrincipal SecurityUserDetails userInfo,
-                                               @RequestBody @NotNull String password) {
+                                               @RequestBody @NotBlank @ValidPassword String password) {
         resetPasswordUsecase.resetPasswordAndActivateMember(userInfo.getUserId(), password);
     }
 
     @Operation(summary = "비밀번호 재설정 API", description = "swagger에서 따옴표를 포함하지 않고 요청합니다.")
     @PatchMapping("/members/password")
     public void resetPassword(@AuthenticationPrincipal SecurityUserDetails userInfo,
-                                               @RequestBody @Valid String password) {
+                                               @RequestBody @NotBlank @ValidPassword String password) {
         resetPasswordUsecase.resetPassword(userInfo.getUserId(), password);
     }
 
