d1992c8207afbd7f7f93ccbf84fb4e7672fd1003
joowojr <joowojr@gmail.com>
2025-01-24 02:41:06 +0900

    CLAP-61 Fix: 리프레시 토큰 도메인 모델로 매핑되지 않는 오류 수정
    <footer>
    - 관련: #21

--- src/main/java/clap/server/adapter/outbound/infrastructure/redis/refresh/RefreshTokenMapper.java
+++ src/main/java/clap/server/adapter/outbound/infrastructure/redis/refresh/RefreshTokenMapper.java
@@ -1,12 +1,10 @@
 package clap.server.adapter.outbound.infrastructure.redis.refresh;
 
-
-import clap.server.adapter.outbound.persistense.mapper.MemberPersistenceMapper;
 import clap.server.domain.model.auth.RefreshToken;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring", uses = {MemberPersistenceMapper.class})
+@Mapper(componentModel = "spring")
 public interface RefreshTokenMapper {
     @InheritInverseConfiguration
     RefreshToken toDomain(final RefreshTokenEntity entity);
