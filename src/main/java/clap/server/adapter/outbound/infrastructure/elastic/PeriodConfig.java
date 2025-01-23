73bd762c533e0ebd5f737d161189d543427b1576
hyoseong-Choi <gytjd9516@naver.com>
2025-01-24 02:18:35 +0900

    CLAP-111 Refactor : 통계 조회 API 리팩토링 및 기능 수정
    <footer>
    - 관련: #74

--- src/main/java/clap/server/adapter/outbound/infrastructure/elastic/PeriodConfig.java
+++ src/main/java/clap/server/adapter/outbound/infrastructure/elastic/PeriodConfig.java
@@ -4,15 +4,27 @@ import co.elastic.clients.elasticsearch._types.aggregations.CalendarInterval;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 @Getter
 @RequiredArgsConstructor
 public enum PeriodConfig {
-    DAY(1, CalendarInterval.Hour, 11, 19),
-    WEEK(14, CalendarInterval.Day, 0, 10);
+    DAY(1, CalendarInterval.Hour, 11, 16),
+    WEEK(7, CalendarInterval.Day, 0, 10),
+    MONTH(-1, CalendarInterval.Day, 0, 10);
 
     private final long daysToSubtract;
     private final CalendarInterval calendarInterval;
     private final int substringStart;
     private final int substringEnd;
+
+    // MONTH에 대해 동적으로 daysToSubtract를 계산하는 메서드
+    public long getDaysToSubtract() {
+        if (this == MONTH) {
+            return ChronoUnit.DAYS.between(LocalDate.now().minusMonths(1), LocalDate.now());
+        }
+        return daysToSubtract;
+    }
 
 }
