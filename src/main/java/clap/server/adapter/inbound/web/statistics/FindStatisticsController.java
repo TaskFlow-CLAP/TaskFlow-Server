2a9eed46968ffa574e9456b6c4149bbcca61ef04
hyoseong-Choi <gytjd9516@naver.com>
2025-01-24 16:03:51 +0900

    CLAP-111 Refactor : 통계 조회 API 주소 통합, 리팩토링, 예외처리
    <footer>
    - 관련: #74

--- src/main/java/clap/server/adapter/inbound/web/statistics/FindStatisticsController.java
+++ src/main/java/clap/server/adapter/inbound/web/statistics/FindStatisticsController.java
@@ -1,7 +1,11 @@
 package clap.server.adapter.inbound.web.statistics;
 
+import clap.server.adapter.inbound.web.dto.statistics.PeriodType;
+import clap.server.adapter.inbound.web.dto.statistics.StatisticsResponse;
+import clap.server.adapter.inbound.web.dto.statistics.StatisticsType;
 import clap.server.application.port.inbound.statistics.*;
 import clap.server.common.annotation.architecture.WebAdapter;
+import clap.server.exception.StatisticsException;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Map;
+import java.util.List;
+
+import static clap.server.exception.code.StatisticsErrorCode.STATISTICS_BAD_REQUEST;
 
 @Tag(name = "작업 관련 통계")
 @WebAdapter
@@ -20,30 +26,49 @@ public class FindStatisticsController {
     private final FindPeriodTaskProcessUsecase findPeriodTaskProcessUsecase;
     private final FindCategoryTaskRequestUsecase findCategoryTaskRequestUsecase;
     private final FindSubCategoryTaskRequestUsecase findSubCategoryTaskRequestUsecase;
-    private final ManagerTaskProcessUsecase managerTaskProcessUsecase;
+    private final FindManagerTaskProcessUsecase findManagerTaskProcessUsecase;
 
-    @GetMapping(value = "/task-requests-by-period")
-    public ResponseEntity<Map<String, Long>> aggregatePeriodTaskRequest(@RequestParam String period) {
-        return ResponseEntity.ok(findPeriodTaskRequestUsecase.aggregatePeriodTaskRequest(period));
+    @GetMapping
+    public ResponseEntity<List<StatisticsResponse>> aggregateTaskStatistics(@RequestParam PeriodType periodType, @RequestParam StatisticsType statisticsType) {
+        switch (statisticsType) {
+            case REQUEST_BY_PERIOD ->
+                    ResponseEntity.ok(findPeriodTaskRequestUsecase
+                    .aggregatePeriodTaskRequest(periodType.getType())
+                    .entrySet()
+                    .stream()
+                    .map(result -> new StatisticsResponse(result.getKey(), result.getValue()))
+                    .toList());
+            case PROCESS_BY_PERIOD ->
+                    ResponseEntity.ok(findPeriodTaskProcessUsecase
+                    .aggregatePeriodTaskProcess(periodType.getType())
+                    .entrySet()
+                    .stream()
+                    .map(result -> new StatisticsResponse(result.getKey(), result.getValue()))
+                    .toList());
+            case REQUEST_BY_CATEGORY ->
+                    ResponseEntity.ok(findCategoryTaskRequestUsecase.aggregateCategoryTaskRequest(periodType.getType())
+                            .entrySet()
+                            .stream()
+                            .map(result -> new StatisticsResponse(result.getKey(), result.getValue()))
+                            .toList());
+            case PROCESS_BY_MANAGER ->
+                    ResponseEntity.ok(findManagerTaskProcessUsecase
+                    .aggregateManagerTaskProcess(periodType.getType())
+                    .entrySet()
+                    .stream()
+                    .map(result -> new StatisticsResponse(result.getKey(), result.getValue()))
+                    .toList());
+        }
+        throw new StatisticsException(STATISTICS_BAD_REQUEST);
     }
 
-    @GetMapping("/task-processed-by-period")
-    public ResponseEntity<Map<String, Long>> aggregatePeriodTaskProcess(@RequestParam String period) {
-        return ResponseEntity.ok(findPeriodTaskProcessUsecase.aggregatePeriodTaskProcess(period));
-    }
-
-    @GetMapping("/task-requests-by-category")
-    public ResponseEntity<Map<String, Long>> aggregateCategoryTaskRequest(@RequestParam String period) {
-        return ResponseEntity.ok(findCategoryTaskRequestUsecase.aggregateCategoryTaskRequest(period));
-    }
-
-    @GetMapping("/task-requests-by-subcategory")
-    public ResponseEntity<Map<String, Long>> aggregateSubCategoryTaskRequest(@RequestParam String period, @RequestParam String mainCategory) {
-        return ResponseEntity.ok(findSubCategoryTaskRequestUsecase.aggregateSubCategoryTaskRequest(period, mainCategory));
-    }
-
-    @GetMapping("/tasks-processed-by-manager")
-    public ResponseEntity<Map<String, Long>> aggregateSubCategoryTaskRequest(@RequestParam String period) {
-        return ResponseEntity.ok(managerTaskProcessUsecase.aggregateManagerTaskProcess(period));
+    @GetMapping("/subcategory")
+    public ResponseEntity<List<StatisticsResponse>> aggregateSubCategoryTaskRequest(@RequestParam String period, @RequestParam String mainCategory) {
+        return ResponseEntity.ok(findSubCategoryTaskRequestUsecase
+                .aggregateSubCategoryTaskRequest(period, mainCategory)
+                .entrySet()
+                .stream()
+                .map(result -> new StatisticsResponse(result.getKey(), result.getValue()))
+                .toList());
     }
 }
