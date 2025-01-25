3773dfd68858fca82ef8aa50280a3556b950aab2
joowojr <joowojr@gmail.com>
2025-01-25 09:11:43 +0900

    CLAP-109 Feat: 로그인 실패 시도를 기록하고 관리하는 service 구현
    <footer>
    - 관련: #59

--- src/main/java/clap/server/application/service/auth/AuthService.java
+++ src/main/java/clap/server/application/service/auth/AuthService.java
@@ -9,12 +9,14 @@ import clap.server.application.port.outbound.member.LoadMemberPort;
 import clap.server.common.annotation.architecture.ApplicationService;
 import clap.server.domain.model.auth.CustomJwts;
 import clap.server.domain.model.member.Member;
-import clap.server.exception.ApplicationException;
-import clap.server.exception.code.MemberErrorCode;
+import clap.server.exception.AuthException;
+import clap.server.exception.code.AuthErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @ApplicationService
 @RequiredArgsConstructor
 class AuthService implements AuthUsecase {
@@ -22,34 +24,40 @@ class AuthService implements AuthUsecase {
     private final CommandRefreshTokenPort commandRefreshTokenPort;
     private final IssueTokenService issueTokenService;
     private final PasswordEncoder passwordEncoder;
+    private final LoginAttemptService loginAttemptService;
 
     @Override
     @Transactional
-    public LoginResponse login(String nickname, String password) {
-        Member member = loadMemberPort.findByNickname(nickname).orElseThrow(
-                () -> new ApplicationException(MemberErrorCode.MEMBER_NOT_FOUND));
-        validatePassword(password, member.getPassword());
+    public LoginResponse login(String nickname, String password, String sessionId, String clientIp) {
+        Member member = getMember(nickname, sessionId, clientIp);
+
+        validatePassword(password, member.getPassword(), sessionId, nickname, clientIp);
 
         if (member.getStatus().equals(MemberStatus.APPROVAL_REQUEST)) {
             String temporaryToken = issueTokenService.issueTemporaryToken(member.getMemberId());
-            return AuthResponseMapper.toLoginResponse(
-                    temporaryToken, null, member
-            );
-        } else {
-            CustomJwts jwtTokens = issueTokenService.issueTokens(member);
-            commandRefreshTokenPort.save(
-                issueTokenService.issueRefreshToken(member.getMemberId())
-            );
-            return AuthResponseMapper.toLoginResponse(
-                    jwtTokens.accessToken(), jwtTokens.refreshToken(), member
-            );
+            return AuthResponseMapper.toLoginResponse(temporaryToken, null, member);
         }
+
+        CustomJwts jwtTokens = issueTokenService.issueTokens(member);
+        commandRefreshTokenPort.save(issueTokenService.issueRefreshToken(member.getMemberId()));
+        loginAttemptService.resetFailedAttempts(sessionId);
+        return AuthResponseMapper.toLoginResponse(jwtTokens.accessToken(), jwtTokens.refreshToken(), member);
     }
 
-    private void validatePassword(String inputPassword, String encodedPassword) {
+    private Member getMember(String inputNickname, String sessionId, String clientIp) {
+        return loadMemberPort.findByNickname(inputNickname).orElseThrow(() ->
+        {
+            loginAttemptService.recordFailedAttempt(sessionId, clientIp, inputNickname);
+            return new AuthException(AuthErrorCode.LOGIN_REQUEST_FAILED);
+        });
+    }
+
+    private void validatePassword(String inputPassword, String encodedPassword, String sessionId, String inputNickname, String clientIp) {
         if (!passwordEncoder.matches(inputPassword, encodedPassword)) {
-            throw new ApplicationException(MemberErrorCode.MEMBER_NOT_FOUND);
+            loginAttemptService.recordFailedAttempt(sessionId, clientIp, inputNickname);
+            throw new AuthException(AuthErrorCode.LOGIN_REQUEST_FAILED);
         }
     }
 
 }
+
---
+++ src/main/java/clap/server/application/service/auth/LoginAttemptService.java
@@ -0,0 +1,61 @@
+package clap.server.application.service.auth;
+
+import clap.server.application.port.outbound.auth.CommandLoginLogPort;
+import clap.server.application.port.outbound.auth.LoadLoginLogPort;
+import clap.server.domain.model.auth.LoginLog;
+import clap.server.exception.AuthException;
+import clap.server.exception.code.AuthErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class LoginAttemptService {
+    private final LoadLoginLogPort loadLoginLogPort;
+    private final CommandLoginLogPort commandLoginLogPort;
+    private static final int MAX_FAILED_ATTEMPTS = 5;
+    private static final long LOCK_TIME_DURATION = 30 * 60 * 1000; // 30분 (밀리초)
+
+    public void recordFailedAttempt(String sessionId, String clientIp, String attemptNickname) {
+        LoginLog loginLog = loadLoginLogPort.findBySessionId(sessionId).orElse(null);
+        if (loginLog == null) {
+            loginLog = LoginLog.createLoginLog(sessionId, clientIp, attemptNickname);
+        } else {
+            int attemptCount = loginLog.recordFailedAttempt();
+            if (attemptCount >= MAX_FAILED_ATTEMPTS) {
+                loginLog.setLocked(true);
+                commandLoginLogPort.save(loginLog);
+                throw new AuthException(AuthErrorCode.ACCOUNT_IS_LOCKED);
+            }
+        }
+        commandLoginLogPort.save(loginLog);
+    }
+
+    public void checkAccountIsLocked(String sessionId) {
+        LoginLog loginLog = loadLoginLogPort.findBySessionId(sessionId).orElse(null);
+        if (loginLog == null) {
+            return;
+        }
+
+        if (loginLog.isLocked()) {
+            LocalDateTime lastAttemptAt = loginLog.getLastAttemptAt();
+            LocalDateTime now = LocalDateTime.now();
+
+            long minutesSinceLastAttempt = ChronoUnit.MINUTES.between(lastAttemptAt, now);
+
+            if (minutesSinceLastAttempt <= LOCK_TIME_DURATION) {
+                throw new AuthException(AuthErrorCode.ACCOUNT_IS_LOCKED);
+            }
+        }
+    }
+
+
+    public void resetFailedAttempts(String sessionId) {
+        commandLoginLogPort.deleteById(sessionId);
+    }
+}
