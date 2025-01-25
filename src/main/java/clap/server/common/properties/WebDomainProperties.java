e7fc065c319d51fec5cff557abaa7a07b2825f34
joowojr <joowojr@gmail.com>
2025-01-25 14:57:18 +0900

    CLAP-133 Config: 웹/서버 도메인 properties 추가
    <footer>
    - 관련: #98

--- src/main/java/clap/server/common/properties/WebDomainProperties.java
+++ src/main/java/clap/server/common/properties/WebDomainProperties.java
@@ -2,14 +2,12 @@ package clap.server.common.properties;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Getter
-@Setter
 @RequiredArgsConstructor
-@ConfigurationProperties(prefix = "password.policy")
-public class PasswordPolicyProperties {
-    private String characters;
-    private int length;
+@ConfigurationProperties(prefix = "web.domain")
+public class WebDomainProperties {
+    private final String local;
+    private final String service;
 }
