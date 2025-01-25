241af80de83c893aea1e0cb078032d20016d7d64
joowojr <joowojr@gmail.com>
2025-01-25 09:16:02 +0900

    CLAP-109 Feat: request에 session id 추가 및 client ip 파싱 추가
    <footer>
    - 관련: #59

--- src/main/java/clap/server/adapter/inbound/web/auth/AuthController.java
+++ src/main/java/clap/server/adapter/inbound/web/auth/AuthController.java
@@ -6,12 +6,18 @@ import clap.server.application.port.inbound.auth.AuthUsecase;
 import clap.server.common.annotation.architecture.WebAdapter;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static clap.server.common.utils.ClientIpParseUtil.getClientIp;
+
+@Slf4j
 @Tag(name = "로그인 / 로그아웃")
 @WebAdapter
 @RequiredArgsConstructor
@@ -21,8 +27,13 @@ public class AuthController {
 
     @Operation(summary = "로그인 API")
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
-        return ResponseEntity.ok(authUsecase.login(request.nickname(), request.password()));
+    public ResponseEntity<LoginResponse> login(@RequestHeader(name = "sessionId") String sessionId,
+                                               @RequestBody LoginRequest request,
+                                               HttpServletRequest httpRequest) {
+        String clientIp = getClientIp(httpRequest);
+        LoginResponse response = authUsecase.login(request.nickname(), request.password(), sessionId, clientIp);
+        return ResponseEntity.ok(response);
     }
+
 
 }
