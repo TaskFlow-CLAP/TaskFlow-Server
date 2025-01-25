81d3ce775cf7582e5b4c24dc3931290183bf197e
joowojr <joowojr@gmail.com>
2025-01-25 09:14:03 +0900

    CLAP-109 Feat: security config 재설정 및 adapter config와 통합
    <footer>
    - 관련: #59

--- src/main/java/clap/server/config/security/SecurityFilterConfig.java
+++ src/main/java/clap/server/config/security/SecurityFilterConfig.java
@@ -1,8 +1,10 @@
 package clap.server.config.security;
 
-import clap.server.application.port.outbound.auth.JwtProvider;
+import clap.server.adapter.inbound.security.LoginAttemptFilter;
 import clap.server.adapter.inbound.security.filter.JwtAuthenticationFilter;
 import clap.server.adapter.inbound.security.filter.JwtExceptionFilter;
+import clap.server.application.port.outbound.auth.JwtProvider;
+import clap.server.application.service.auth.LoginAttemptService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +16,7 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class SecurityFilterConfig {
 	private final UserDetailsService securityUserDetails;
+	private final LoginAttemptService loginAttemptService;
 	private final JwtProvider accessTokenProvider;
 	private final JwtProvider temporaryTokenProvider;
 	private final AccessDeniedHandler accessDeniedHandler;
@@ -26,5 +29,10 @@ public class SecurityFilterConfig {
 	@Bean
 	public JwtAuthenticationFilter jwtAuthenticationFilter() {
 		return new JwtAuthenticationFilter(securityUserDetails, accessTokenProvider, temporaryTokenProvider, accessDeniedHandler);
+	}
+
+	@Bean
+	public LoginAttemptFilter loginAttemptFilter() {
+		return new LoginAttemptFilter(loginAttemptService);
 	}
 }
