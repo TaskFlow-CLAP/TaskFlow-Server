0feb54c58df7116604f8828cb9fa453bb0476756
joowojr <joowojr@gmail.com>
2025-01-25 17:33:51 +0900

    CLAP-133 Refactor: properties class 방식에서 @value를 통해 값 주입
    <footer>
    - 관련: #98

--- src/main/java/clap/server/config/security/CorsConfig.java
+++ src/main/java/clap/server/config/security/CorsConfig.java
@@ -1,8 +1,6 @@
 package clap.server.config.security;
 
-import clap.server.common.properties.ServerDomainProperties;
-import clap.server.common.properties.WebDomainProperties;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -15,17 +13,24 @@ import java.util.List;
 import static clap.server.common.constants.AuthConstants.SESSION_ID;
 
 @Configuration
-@RequiredArgsConstructor
 public class CorsConfig {
-    private final ServerDomainProperties serverDomainProperties;
-    private final WebDomainProperties webDomainProperties;
+    @Value("${server.domain.local}")
+    private String localServerDomain;
+
+    @Value("${server.domain.service}")
+    private String serviceServerDomain;
+
+    @Value("${web.domain.local}")
+    private String localWebDomain;
+
+    @Value("${web.domain.service}")
+    private String serviceWebDomain;
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
-                List.of(serverDomainProperties.getLocal(), serverDomainProperties.getService(),
-                        webDomainProperties.getLocal(), webDomainProperties.getService()));
+                List.of(localServerDomain, localWebDomain, serviceServerDomain, serviceWebDomain));
         configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of(HttpHeaders.AUTHORIZATION, SESSION_ID.getValue()));
