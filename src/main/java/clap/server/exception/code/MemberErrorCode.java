e7ac82b9aff7c2934b412c09f71949968367b227
joowojr <joowojr@gmail.com>
2025-01-26 16:30:34 +0900

    CLAP-140 Refactor: 회원 생성/수정 시에 담당자만 리뷰어가 될 수 있도록 검증하는 로직 추가
    <footer>
    - 관련: #111

--- src/main/java/clap/server/exception/code/MemberErrorCode.java
+++ src/main/java/clap/server/exception/code/MemberErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum MemberErrorCode implements BaseErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_001", "회원을 찾을 수 없습니다."),
     ACTIVE_MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_002", "활성화 회원을 찾을 수 없습니다."),
+    MEMBER_REGISTER_FAILED(HttpStatus.BAD_REQUEST, "MEMBER_003", "회원 등록에 실패하였습니다");
     ;
 
     private final HttpStatus httpStatus;
