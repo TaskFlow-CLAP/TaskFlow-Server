6c50513a468e28b0684d47bbd3d09446d445dd5b
andrew <leegd120@gmail.com>
2025-01-26 21:41:44 +0900

    CLAP-145 Fix: 반환값을 Page가 Slice로 설정

--- src/main/java/clap/server/application/port/inbound/notification/FindNotificationListUsecase.java
+++ src/main/java/clap/server/application/port/inbound/notification/FindNotificationListUsecase.java
@@ -1,9 +1,9 @@
 package clap.server.application.port.inbound.notification;
 
+import clap.server.adapter.inbound.web.dto.common.SliceResponse;
 import clap.server.adapter.inbound.web.dto.notification.FindNotificationListResponse;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface FindNotificationListUsecase {
-    Page<FindNotificationListResponse> findNotificationList(Long receiverId, Pageable pageable);
+    SliceResponse<FindNotificationListResponse> findNotificationList(Long receiverId, Pageable pageable);
 }
