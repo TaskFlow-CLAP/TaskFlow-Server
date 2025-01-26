30916e228845e0863d7b2b1664b2fc83110c97b0
andrew <leegd120@gmail.com>
2025-01-27 01:24:12 +0900

    CLAP-150 Fix: 알림 ErrorCode 추가

--- src/main/java/clap/server/exception/code/NotificationErrorCode.java
+++ src/main/java/clap/server/exception/code/NotificationErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum NotificationErrorCode implements BaseErrorCode {
 
     NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTIFICATION_001", "알림을 찾을 수 없습니다"),
+    SSE_SEND_FAILED(HttpStatus.BAD_REQUEST, "NOTIFICATION_002", "SSE 초기 연결에 실패하였습니다"),
     ;
 
     private final HttpStatus httpStatus;
