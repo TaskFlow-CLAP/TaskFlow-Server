20fa0dcc56a1cc23503ed8dbeccbd85d0015110c
andrew <leegd120@gmail.com>
2025-01-26 17:34:25 +0900

    CLAP-136 Fix: ErrorCode 생성 및 Class 명 수정

--- src/main/java/clap/server/application/service/notification/ReadNotificationService.java
+++ src/main/java/clap/server/application/service/notification/ReadNotificationService.java
@@ -1,20 +1,18 @@
 package clap.server.application.service.notification;
 
-import clap.server.adapter.outbound.persistense.entity.notification.NotificationEntity;
-import clap.server.adapter.outbound.persistense.repository.notification.NotificationRepository;
 import clap.server.application.port.inbound.notification.UpdateNotificationUsecase;
 import clap.server.application.port.outbound.notification.CommandNotificationPort;
 import clap.server.application.port.outbound.notification.LoadNotificationPort;
 import clap.server.common.annotation.architecture.ApplicationService;
 import clap.server.domain.model.notification.Notification;
+import clap.server.exception.ApplicationException;
+import clap.server.exception.code.NotificationErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @ApplicationService
 @RequiredArgsConstructor
-public class UpdateNotificationService implements UpdateNotificationUsecase {
+public class ReadNotificationService implements UpdateNotificationUsecase {
 
     private final LoadNotificationPort loadNotificationPort;
     private final CommandNotificationPort commandNotificationPort;
@@ -23,7 +21,8 @@ public class UpdateNotificationService implements UpdateNotificationUsecase {
     @Transactional
     @Override
     public void updateNotification(Long notificationId) {
-        Notification notification = loadNotificationPort.findById(notificationId).get();
+        Notification notification = loadNotificationPort.findById(notificationId)
+                .orElseThrow(() -> new ApplicationException(NotificationErrorCode.NOTIFICATION_NOT_FOUND));
         notification.updateNotificationIsRead();
         commandNotificationPort.save(notification);
     }
