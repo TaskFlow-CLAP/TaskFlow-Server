342c9d85d91230fa9925e5d1dd2d5ccf71812870
andrew <leegd120@gmail.com>
2025-01-27 04:06:31 +0900

    CLAP-150 Fix: Error Code 추가 설정

--- src/main/java/clap/server/exception/code/NotificationErrorCode.java
+++ src/main/java/clap/server/exception/code/NotificationErrorCode.java
@@ -10,6 +10,9 @@ public enum NotificationErrorCode implements BaseErrorCode {
 
     NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTIFICATION_001", "알림을 찾을 수 없습니다"),
     SSE_SEND_FAILED(HttpStatus.BAD_REQUEST, "NOTIFICATION_002", "SSE 초기 연결에 실패하였습니다"),
+    EMAIL_SEND_FAILED(HttpStatus.BAD_REQUEST, "NOTIFICATION_003", "이메일 알림 전송에 실패하였습니다"),
+    KAKAO_SEND_FAILED(HttpStatus.BAD_REQUEST, "NOTIFICATION_004", "카카오워크 알림 전송에 실패하였습니다"),
+    AGIT_SEND_FAILED(HttpStatus.BAD_REQUEST, "NOTIFICATION_005", "아지트 알림 전송에 실패하였습니다"),
     ;
 
     private final HttpStatus httpStatus;
