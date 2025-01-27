f1f035a885352a0d804906b9ff72d0d262f48c03
joowojr <joowojr@gmail.com>
2025-01-28 07:56:37 +0900

    CLAP-70 Fix: untilDate 파라미터가 null일 경우 처리
    <footer>
    - 관련: #29

--- src/main/java/clap/server/application/Task/TaskBoardService.java
+++ src/main/java/clap/server/application/Task/TaskBoardService.java
@@ -1,12 +1,16 @@
 package clap.server.application.Task;
 
+import clap.server.adapter.inbound.web.dto.task.request.UpdateTaskOrderRequest;
 import clap.server.adapter.inbound.web.dto.task.response.TaskBoardResponse;
 import clap.server.adapter.outbound.persistense.entity.task.constant.TaskStatus;
 import clap.server.application.mapper.TaskMapper;
 import clap.server.application.port.inbound.domain.MemberService;
+import clap.server.application.port.inbound.domain.TaskService;
 import clap.server.application.port.inbound.task.TaskBoardUsecase;
+import clap.server.application.port.outbound.task.CommandTaskPort;
 import clap.server.application.port.outbound.task.LoadTaskPort;
 import clap.server.common.annotation.architecture.ApplicationService;
+import clap.server.domain.model.member.Member;
 import clap.server.domain.model.task.Task;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +18,7 @@ import org.springframework.data.domain.Slice;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @ApplicationService
@@ -25,13 +30,16 @@ class TaskBoardService implements TaskBoardUsecase {
             TaskStatus.COMPLETED
     );
     private final MemberService memberService;
+    private final TaskService taskService;
     private final LoadTaskPort loadTaskPort;
+    private final CommandTaskPort commandTaskPort;
 
     @Transactional(readOnly = true)
     @Override
     public TaskBoardResponse getTaskBoards(Long processorId, LocalDate untilDate, Pageable pageable) {
         memberService.findById(processorId);
-        Slice<Task> tasks = loadTaskPort.findByProcessorAndStatus(processorId, VIEWABLE_STATUSES, untilDate.plusDays(1).atStartOfDay(), pageable);
+        LocalDateTime untilDateTime = untilDate==null ? LocalDate.now().plusDays(1).atStartOfDay() : untilDate.plusDays(1).atStartOfDay();
+        Slice<Task> tasks = loadTaskPort.findByProcessorAndStatus(processorId, VIEWABLE_STATUSES, untilDateTime, pageable);
         return TaskMapper.toSliceTaskItemResponse(tasks);
     }
 
