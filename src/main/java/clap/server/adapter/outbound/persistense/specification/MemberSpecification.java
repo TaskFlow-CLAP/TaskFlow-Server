f1d307521ca777db286f6e350e17a00b31f5a97d
Sihun23 <gcsw16@Gachonui-MacBookPro-2.local>
2025-01-29 04:37:52 +0900

    CLAP-118 fix:'삭제'상태의 회원은 조회에서 제외

--- src/main/java/clap/server/adapter/outbound/persistense/specification/MemberSpecification.java
+++ src/main/java/clap/server/adapter/outbound/persistense/specification/MemberSpecification.java
@@ -2,13 +2,23 @@ package clap.server.adapter.outbound.persistense.specification;
 
 import clap.server.adapter.inbound.web.dto.admin.FindMemberRequest;
 import clap.server.adapter.outbound.persistense.entity.member.MemberEntity;
+import clap.server.adapter.outbound.persistense.entity.member.constant.MemberStatus;
 import org.springframework.data.jpa.domain.Specification;
 
 public class MemberSpecification {
 
+    // DELETED 상태 제외
+    public static Specification<MemberEntity> isNotDeleted() {
+        return (root, query, cb) -> cb.notEqual(root.get("status"), MemberStatus.DELETED);
+    }
+
     public static Specification<MemberEntity> withFilter(FindMemberRequest filterRequest) {
         return (root, query, cb) -> {
             var predicates = cb.conjunction();
+
+            // DELETED 상태가 아닌 회원만 기본으로 조회
+            predicates = cb.and(predicates, isNotDeleted().toPredicate(root, query, cb));
+
 
             if (filterRequest.getName() != null) {
                 predicates = cb.and(predicates, cb.like(root.get("name"), "%" + filterRequest.getName() + "%"));
