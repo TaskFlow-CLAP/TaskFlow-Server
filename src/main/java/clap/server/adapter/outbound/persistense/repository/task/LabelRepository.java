6c665b912f11dd8046aa40c60cf3e0400eb2ae5e
andrew <leegd120@gmail.com>
2025-01-29 04:50:59 +0900

    CLAP-165 Feat: 관리자용 구분 목록 조회 API 구현

--- src/main/java/clap/server/adapter/outbound/persistense/repository/task/LabelRepository.java
+++ src/main/java/clap/server/adapter/outbound/persistense/repository/task/LabelRepository.java
@@ -1,9 +1,17 @@
 package clap.server.adapter.outbound.persistense.repository.task;
 
 import clap.server.adapter.outbound.persistense.entity.task.LabelEntity;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LabelRepository extends JpaRepository<LabelEntity, Long> {
+
+    List<LabelEntity> findByIsDeletedFalse();
+
+    Slice<LabelEntity> findByIsDeletedFalse(Pageable pageable);
 }
