0ff4b3b9fb2f3e11ef504cd75f70532e1c318cd1
hyoseong-Choi <gytjd9516@naver.com>
2025-01-28 16:35:19 +0900

    CLAP-148 Feature : 카테고리 삭제 API 수정
    <footer>
    - 관련: #120

--- src/main/java/clap/server/adapter/inbound/web/admin/DeleteCategoryController.java
+++ src/main/java/clap/server/adapter/inbound/web/admin/DeleteCategoryController.java
@@ -8,7 +8,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,7 +20,7 @@ public class DeleteCategoryController {
     private final DeleteCategoryUsecase deleteCategoryUsecase;
 
     @Operation(summary = "카테고리 삭제")
-    @PatchMapping("/categories/{categoryId}")
+    @DeleteMapping("/categories/{categoryId}")
     @Secured("ROLE_ADMIN")
     public void deleteCategory(@AuthenticationPrincipal SecurityUserDetails userInfo, @PathVariable Long categoryId) {
         deleteCategoryUsecase.deleteCategory(userInfo.getUserId(), categoryId);
