b0f1a023b101c2b5fc967b9baf1469d576f58a1b
andrew <leegd120@gmail.com>
2025-01-28 17:23:19 +0900

    Revert "CLAP-159 Feat: 작업 구분 변경 기능 구현"
    This reverts commit 31290b57afa3fafae42340d54f0b156e5bb25340.

--- src/main/java/clap/server/application/Task/UpdateTaskService.java
+++ src/main/java/clap/server/application/Task/UpdateTaskService.java
@@ -1,26 +1,29 @@
 package clap.server.application.Task;
 
-import clap.server.adapter.inbound.web.dto.task.*;
+import clap.server.adapter.inbound.web.dto.task.UpdateTaskRequest;
+import clap.server.adapter.inbound.web.dto.task.UpdateTaskResponse;
+import clap.server.adapter.inbound.web.dto.task.UpdateTaskStatusRequest;
+import clap.server.adapter.inbound.web.dto.task.UpdateTaskProcessorRequest;
 import clap.server.adapter.outbound.infrastructure.s3.S3UploadAdapter;
 import clap.server.application.mapper.AttachmentMapper;
 import clap.server.application.mapper.TaskMapper;
 import clap.server.application.port.inbound.domain.CategoryService;
 import clap.server.application.port.inbound.domain.MemberService;
 import clap.server.application.port.inbound.domain.TaskService;
-import clap.server.application.port.inbound.task.UpdateTaskLabelUsecase;
 import clap.server.application.port.inbound.task.UpdateTaskProcessorUsecase;
 import clap.server.application.port.inbound.task.UpdateTaskStatusUsecase;
 import clap.server.application.port.inbound.task.UpdateTaskUsecase;
 import clap.server.application.port.outbound.task.CommandAttachmentPort;
 import clap.server.application.port.outbound.task.CommandTaskPort;
 import clap.server.application.port.outbound.task.LoadAttachmentPort;
-import clap.server.application.port.outbound.task.LoadLabelPort;
 import clap.server.common.annotation.architecture.ApplicationService;
 import clap.server.domain.model.member.Member;
-import clap.server.domain.model.task.*;
+import clap.server.domain.model.task.Attachment;
+import clap.server.domain.model.task.Category;
+import clap.server.domain.model.task.FilePath;
+import clap.server.domain.model.task.Task;
 
 import clap.server.exception.ApplicationException;
-import clap.server.exception.code.LabelErrorCode;
 import clap.server.exception.code.MemberErrorCode;
 import clap.server.exception.code.TaskErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -31,20 +34,17 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 import java.util.Objects;
 
-import static clap.server.exception.code.MemberErrorCode.ACTIVE_MEMBER_NOT_FOUND;
-
 
 @ApplicationService
 @RequiredArgsConstructor
 @Slf4j
-public class UpdateTaskService implements UpdateTaskUsecase, UpdateTaskStatusUsecase, UpdateTaskProcessorUsecase, UpdateTaskLabelUsecase {
+public class UpdateTaskService implements UpdateTaskUsecase, UpdateTaskStatusUsecase, UpdateTaskProcessorUsecase {
 
     private final MemberService memberService;
     private final CategoryService categoryService;
     private final TaskService taskService;
     private final CommandTaskPort commandTaskPort;
     private final LoadAttachmentPort loadAttachmentPort;
-    private final LoadLabelPort loadLabelPort;
     private final CommandAttachmentPort commandAttachmentPort;
     private final S3UploadAdapter s3UploadAdapter;
 
@@ -94,21 +94,6 @@ public class UpdateTaskService implements UpdateTaskUsecase, UpdateTaskStatusUse
         return TaskMapper.toUpdateTaskResponse(updateTask);
 
         // TODO : 알림 생성 로직 및 푸시 알림 로직 추가
-    }
-
-    @Transactional
-    @Override
-    public UpdateTaskResponse updateTaskLabel(Long taskId, Long userId, UpdateTaskLabelRequest request) {
-        Member reviewer = memberService.findActiveMember(userId);
-        if (!reviewer.isReviewer()) {
-            throw new ApplicationException(MemberErrorCode.NOT_A_REVIEWER);
-        }
-        Task task = taskService.findById(taskId);
-        Label label = loadLabelPort.findById(request.labelId()).orElseThrow(() -> new ApplicationException(LabelErrorCode.LABEL_NOT_FOUND));
-
-        task.updateLabel(label);
-        Task updatetask = commandTaskPort.save(task);
-        return TaskMapper.toUpdateTaskResponse(updatetask);
     }
 
     private void updateAttachments(List<Long> attachmentIdsToDelete, List<MultipartFile> files, Task task) {
--- src/main/java/clap/server/application/port/inbound/task/UpdateTaskLabelUsecase.java
+++
@@ -1,8 +0,0 @@
-package clap.server.application.port.inbound.task;
-
-import clap.server.adapter.inbound.web.dto.task.UpdateTaskLabelRequest;
-import clap.server.adapter.inbound.web.dto.task.UpdateTaskResponse;
-
-public interface UpdateTaskLabelUsecase {
-    UpdateTaskResponse updateTaskLabel(Long taskId, Long userId, UpdateTaskLabelRequest request);
-}
