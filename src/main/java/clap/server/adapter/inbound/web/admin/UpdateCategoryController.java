d24fdf8e0fe0207867011d3aecab1543554acf01
hyoseong-Choi <gytjd9516@naver.com>
2025-01-28 15:15:24 +0900

    CLAP-147 Feature : 카테고리 추가, 수정 API 리뷰반영 수정
    <footer>
    - 관련: #119

--- src/main/java/clap/server/adapter/inbound/web/admin/UpdateCategoryController.java
+++ src/main/java/clap/server/adapter/inbound/web/admin/UpdateCategoryController.java
@@ -10,19 +10,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Tag(name = "카테고리 수정")
 @WebAdapter
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/management")
 public class UpdateCategoryController {
     private final UpdateCategoryUsecase updateCategoryUsecase;
 
     @Operation(summary = "카테고리 수정")
-    @PatchMapping("/category")
+    @PatchMapping("/categories/categoryId")
     @Secured("ROLE_ADMIN")
-    public void updateCategory(@AuthenticationPrincipal SecurityUserDetails userInfo, UpdateCategoryRequest updateCategoryRequest) {
-        updateCategoryUsecase.updateCategory(userInfo.getUserId(), updateCategoryRequest.categoryId(), updateCategoryRequest.name(), updateCategoryRequest.code());
+    public void updateCategory(@AuthenticationPrincipal SecurityUserDetails userInfo, Long categoryId,
+                               @RequestBody UpdateCategoryRequest updateCategoryRequest) {
+        updateCategoryUsecase.updateCategory(userInfo.getUserId(), categoryId, updateCategoryRequest.name(), updateCategoryRequest.code());
     }
 }
