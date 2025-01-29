92b52563d07eeb815283ef416ec27995b9f56a12
joowojr <joowojr@gmail.com>
2025-01-29 22:11:55 +0700

    CLAP-172 Remove: 알림 설정 엔티티 삭제
    <footer>
    - 관련: #133

--- src/main/java/clap/server/adapter/outbound/persistense/repository/member/MemberNotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package clap.server.adapter.outbound.persistense.repository.member;
-
-import clap.server.adapter.outbound.persistense.entity.member.MemberNotificationEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberNotificationRepository extends JpaRepository<MemberNotificationEntity, Long> {
-}
