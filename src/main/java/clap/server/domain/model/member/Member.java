fa2bcc3556b01d275c0c18e6e420c47043393ccf
joowojr <joowojr@gmail.com>
2025-01-29 22:12:29 +0700

    CLAP-172 Feat: 웹훅 및 이메일 알림 설정에 대한 필드 추가
    <footer>
    - 관련: #133

--- src/main/java/clap/server/domain/model/member/Member.java
+++ src/main/java/clap/server/domain/model/member/Member.java
@@ -13,20 +13,16 @@ public class Member extends BaseTime {
     private Long memberId;
     private MemberInfo memberInfo;
     private Member admin;
-    private Boolean kakaoworkNotificationEnabled;
-    private Boolean agitNotificationEnabled;
-    private Boolean emailNotificationEnabled;
+    private Boolean notificationEnabled;
     private String imageUrl;
     private MemberStatus status;
     private String password;
 
     @Builder
-    public Member(MemberInfo memberInfo, Boolean agitNotificationEnabled, Boolean emailNotificationEnabled, Boolean kakaoworkNotificationEnabled,
-                  Member admin, String imageUrl, MemberStatus status, String password) {
+    public Member(MemberInfo memberInfo, Boolean notificationEnabled, Member admin, String imageUrl,
+                  MemberStatus status, String password) {
         this.memberInfo = memberInfo;
-        this.agitNotificationEnabled = agitNotificationEnabled;
-        this.emailNotificationEnabled = emailNotificationEnabled;
-        this.kakaoworkNotificationEnabled = kakaoworkNotificationEnabled;
+        this.notificationEnabled = notificationEnabled;
         this.admin = admin;
         this.imageUrl = imageUrl;
         this.status = status;
@@ -34,16 +30,14 @@ public class Member extends BaseTime {
     }
 
     public static Member createMember(Member admin, MemberInfo memberInfo) {
-        return Member.builder()
-                .memberInfo(memberInfo)
-                .agitNotificationEnabled(null)
-                .emailNotificationEnabled(null)
-                .kakaoworkNotificationEnabled(null)
-                .admin(admin)
-                .imageUrl(null)
-                .status(MemberStatus.PENDING)
-                .password(null)
-                .build();
+       return Member.builder()
+               .memberInfo(memberInfo)
+               .notificationEnabled(null)
+               .admin(admin)
+               .imageUrl(null)
+               .status(MemberStatus.PENDING)
+               .password(null)
+               .build();
     }
 
     public void resetPassword(String newEncodedPassword) {
@@ -53,9 +47,6 @@ public class Member extends BaseTime {
     public void resetPasswordAndActivateMember(String newEncodedPassword) {
         this.password = newEncodedPassword;
         this.status = MemberStatus.ACTIVE;
-        this.agitNotificationEnabled = true;
-        this.emailNotificationEnabled = true;
-        this.kakaoworkNotificationEnabled = true;
     }
 
     public String getNickname() {
@@ -64,16 +55,6 @@ public class Member extends BaseTime {
 
     public boolean isReviewer() {
         return this.memberInfo != null && this.memberInfo.isReviewer();
-    }
-
-    public void updateMemberInfo(String name, Boolean agitNotificationEnabled, Boolean emailNotificationEnabled, Boolean kakaoWorkNotificationEnabled, String imageUrl) {
-        this.memberInfo.updateName(name);
-        this.agitNotificationEnabled = agitNotificationEnabled;
-        this.emailNotificationEnabled = emailNotificationEnabled;
-        this.kakaoworkNotificationEnabled = kakaoWorkNotificationEnabled;
-        if (imageUrl != null) {
-            this.imageUrl = imageUrl;
-        }
     }
 
     public void setStatusDeleted() {
