5af313ca99d4cf610fc5f2235fa05c895f4a9345
Sihun23 <gcsw16@Gachonui-MacBookPro-2.local>
2025-01-29 19:22:49 +0900

    CLAP-116 fix:컨트롤러 태그 수정

--- src/main/java/clap/server/adapter/inbound/web/admin/RegisterMemberCsvController.java
+++ src/main/java/clap/server/adapter/inbound/web/admin/RegisterMemberCsvController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-@Tag(name = "Admin")
+@Tag(name = "05. Admin")
 @WebAdapter
 @RequestMapping("/api/admin")
 public class RegisterMemberCsvController {
