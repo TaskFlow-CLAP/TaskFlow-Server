9acdf3a2f09d7969098f3cd6e7e6c22b446b829d
parkjaehak <atom8426@gmail.com>
2025-01-31 00:29:27 +0900

    CLAP-81 Fix: 감사로그조회 엔드포인트 변경

--- src/main/java/clap/server/adapter/inbound/web/log/LogController.java
+++ src/main/java/clap/server/adapter/inbound/web/log/LogController.java
@@ -1,4 +1,4 @@
-package clap.server.adapter.inbound.web.admin;
+package clap.server.adapter.inbound.web.log;
 
 import clap.server.adapter.inbound.web.dto.admin.AnonymousLogResponse;
 import clap.server.adapter.inbound.web.dto.admin.MemberLogResponse;
@@ -6,6 +6,7 @@ import clap.server.application.port.inbound.log.FindApiLogsUsecase;
 import clap.server.common.annotation.architecture.WebAdapter;
 import clap.server.config.annotation.LogType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,21 +16,21 @@ import java.util.List;
 
 @WebAdapter
 @RestController
-@RequestMapping("/api/management")
+@RequestMapping("/api/logs")
 @RequiredArgsConstructor
 public class LogController {
 
     private final FindApiLogsUsecase getApiLogsUseCase;
 
-    @LogType("로그인 로그")
-    @GetMapping("/login-logs")
+    @Secured({"ROLE_ADMIN"})
+    @GetMapping("/login")
     public List<AnonymousLogResponse> getLoginAttempts() {
         return getApiLogsUseCase.getAnonymousLogs();
     }
 
-    @LogType("일반 로그")
-    @GetMapping("/api-logs")
-    public List<MemberLogResponse> getApiCalls(@RequestParam(defaultValue = "") String logType) {
+    @Secured({"ROLE_ADMIN"})
+    @GetMapping("/general")
+    public List<MemberLogResponse> getApiCalls() {
         return getApiLogsUseCase.getMemberLogs();
     }
 }
