672c0056ecc03bf6e4cc226943134337b4d69f92
joowojr <joowojr@gmail.com>
2025-01-30 13:25:05 +0700

    CLAP-172 Rename: 클래스명 수정
    <footer>
    - 관련: #164

--- src/main/java/clap/server/application/port/inbound/admin/UpdateMemberUsecase.java
+++ src/main/java/clap/server/application/port/inbound/admin/UpdateMemberUsecase.java
@@ -2,6 +2,6 @@ package clap.server.application.port.inbound.admin;
 
 import clap.server.adapter.inbound.web.dto.admin.UpdateMemberInfoRequest;
 
-public interface UpdateMemberInfoUsecase {
+public interface UpdateMemberUsecase {
     void updateMemberInfo(Long adminId, Long memberId, UpdateMemberInfoRequest request);
 }
