4935bafe20ca14c3d61bcb270dd3e3e22b72aacc
joowojr <joowojr@gmail.com>
2025-01-30 07:01:25 +0700

    CLAP-172 Refactor: usecase 세분화
    <footer>
    - 관련: #164

--- src/main/java/clap/server/adapter/inbound/web/member/ResetPasswordController.java
+++ src/main/java/clap/server/adapter/inbound/web/member/ResetPasswordController.java
@@ -1,6 +1,7 @@
 package clap.server.adapter.inbound.web.member;
 
 import clap.server.adapter.inbound.security.SecurityUserDetails;
+import clap.server.application.port.inbound.member.ResetInitialPasswordUsecase;
 import clap.server.application.port.inbound.member.ResetPasswordUsecase;
 import clap.server.common.annotation.architecture.WebAdapter;
 import clap.server.common.annotation.validation.password.ValidPassword;
@@ -19,12 +20,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/api")
 public class ResetPasswordController {
     private final ResetPasswordUsecase resetPasswordUsecase;
+    private final ResetInitialPasswordUsecase resetInitialPasswordUsecase;
 
     @Operation(summary = "초기 로그인 후 비밀번호 재설정 API", description = "swagger에서 따옴표를 포함하지 않고 요청합니다.")
     @PatchMapping("/members/initial-password")
     public void resetPasswordAndActivateMember(@AuthenticationPrincipal SecurityUserDetails userInfo,
                                                @RequestBody @NotBlank @ValidPassword String password) {
-        resetPasswordUsecase.resetPasswordAndActivateMember(userInfo.getUserId(), password);
+        resetInitialPasswordUsecase.resetPasswordAndActivateMember(userInfo.getUserId(), password);
     }
 
     @Operation(summary = "비밀번호 재설정 API", description = "swagger에서 따옴표를 포함하지 않고 요청합니다.")
