c3292fea7429b3498595f3fd37a64875cd3df0ed
hyoseong-Choi <gytjd9516@naver.com>
2025-01-30 21:41:58 +0900

    CLAP-148 Feature : 카테고리 CUD 리뷰 반영 수정, 조회 반환 양식 수정
    <footer>
    - 관련: #120

--- src/main/java/clap/server/adapter/inbound/web/admin/AddCategoryController.java
+++ src/main/java/clap/server/adapter/inbound/web/admin/AddCategoryController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Tag(name = "05. Admin")
 @WebAdapter
 @RequiredArgsConstructor
-@RequestMapping("/api/management")
+@RequestMapping("/api/managements")
 public class AddCategoryController {
     private final AddCategoryUsecase addCategoryUsecase;
 
--- src/main/java/clap/server/application/service/admin/FindAllCategoryService.java
+++ src/main/java/clap/server/application/service/admin/FindAllCategoryService.java
@@ -1,16 +1,15 @@
 package clap.server.application.service.admin;
 
 import clap.server.adapter.inbound.web.dto.admin.FindAllCategoryResponse;
-import clap.server.adapter.inbound.web.dto.admin.FindMainCategoryResponse;
 import clap.server.adapter.inbound.web.dto.admin.FindSubCategoryResponse;
 import clap.server.application.mapper.response.CategoryResponseMapper;
 import clap.server.application.port.inbound.admin.FindAllCategoryUsecase;
 import clap.server.application.port.outbound.task.LoadCategoryPort;
 import clap.server.common.annotation.architecture.ApplicationService;
+import clap.server.domain.model.task.Category;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @ApplicationService
@@ -20,16 +19,23 @@ public class FindAllCategoryService implements FindAllCategoryUsecase {
 
     @Override
     @Transactional(readOnly = true)
-    public FindAllCategoryResponse findAllCategory() {
-        List<FindMainCategoryResponse> mainCategoryResponses = new ArrayList<>();
-        List<FindSubCategoryResponse> subCategoryResponses = new ArrayList<>();
-        loadCategoryPort.findAll().forEach(category -> {
-            if (category.getMainCategory() == null) {
-                mainCategoryResponses.add(CategoryResponseMapper.toFindMainCategoryResponse(category));
-            } else {
-                subCategoryResponses.add(CategoryResponseMapper.toFindSubCategoryResponse(category));
-            }
-        });
-        return CategoryResponseMapper.toFindAllCategoryResponse(mainCategoryResponses, subCategoryResponses);
+    public List<FindAllCategoryResponse> findAllCategory() {
+        List<Category> categories = loadCategoryPort.findAll();
+        return categories.stream().filter(category -> category.getMainCategory() == null)
+                .map(parent -> CategoryResponseMapper.toFindAllCategoryResponse(
+                        parent.getCategoryId(),
+                        parent.getName(),
+                        parent.getCode(),
+                        getSubCategories(categories, parent) // 2차 카테고리 리스트 변환
+                ))
+                .toList();
+    }
+
+    private List<FindSubCategoryResponse> getSubCategories(List<Category> categories, Category parent) {
+        return categories.stream()
+                .filter(category -> category.getMainCategory() != null &&
+                                    category.getMainCategory().getCategoryId().equals(parent.getCategoryId())) // 부모가 같은 것들 필터링
+                .map(CategoryResponseMapper::toFindSubCategoryResponse)
+                .toList();
     }
 }
