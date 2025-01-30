d614fea4adc392ee86906c83c5319e6d9c285cf1
joowojr <joowojr@gmail.com>
2025-01-30 13:13:23 +0700

    CLAP-172 Feat: 회원 정보 상세 조회 API 구현
    <footer>
    - 관련: #164

--- src/main/java/clap/server/application/port/inbound/member/MemberProfileUsecase.java
+++ src/main/java/clap/server/application/port/inbound/member/MemberProfileUsecase.java
@@ -2,6 +2,6 @@ package clap.server.application.port.inbound.member;
 
 import clap.server.adapter.inbound.web.dto.member.MemberProfileResponse;
 
-public interface MemberInfoUsecase {
+public interface MemberProfileUsecase {
     MemberProfileResponse getMemberProfile(Long memberId);
 }
