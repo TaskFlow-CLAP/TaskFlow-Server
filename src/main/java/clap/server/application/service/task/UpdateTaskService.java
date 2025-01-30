a7e55d19bf3b96a2f2c21e8c8937ff26d42846e3
joowojr <joowojr@gmail.com>
2025-01-30 21:03:23 +0700

    CLAP-172 Remove: 불필요한 코드 제거
    <footer>
    - 관련: #164

--- src/main/java/clap/server/application/service/task/UpdateTaskService.java
+++ src/main/java/clap/server/application/service/task/UpdateTaskService.java
@@ -1,7 +1,6 @@
 package clap.server.application.service.task;
 
 import clap.server.adapter.inbound.web.dto.task.*;
-import clap.server.adapter.outbound.infrastructure.s3.S3UploadAdapter;
 import clap.server.adapter.outbound.persistense.entity.task.constant.TaskHistoryType;
 import clap.server.application.mapper.AttachmentMapper;
 import clap.server.application.mapper.TaskMapper;
@@ -45,7 +44,6 @@ public class UpdateTaskService implements UpdateTaskUsecase, UpdateTaskStatusUse
     private final LabelService labelService;
     private final CommandAttachmentPort commandAttachmentPort;
     private final CommandTaskHistoryPort commandTaskHistoryPort;
-    private final S3UploadAdapter s3UploadAdapter;
 
     @Override
     @Transactional
