0aac98dd531574e1c001971b82f670b19e7dda7a
joowojr <joowojr@gmail.com>
2025-01-30 13:16:44 +0700

    CLAP-172 Feat: 회원 정보 수정 API 구현
    <footer>
    - 관련: #164

--- src/main/java/clap/server/application/service/member/UpdateMemberInfoService.java
+++ src/main/java/clap/server/application/service/member/UpdateMemberInfoService.java
@@ -3,14 +3,12 @@ package clap.server.application.service.member;
 import clap.server.adapter.inbound.web.dto.member.UpdateMemberInfoRequest;
 import clap.server.application.port.inbound.domain.MemberService;
 import clap.server.application.port.inbound.member.UpdateMemberInfoUsecase;
-import clap.server.application.port.outbound.member.CommandMemberPort;
 import clap.server.application.port.outbound.s3.S3UploadPort;
 import clap.server.common.annotation.architecture.ApplicationService;
 import clap.server.common.constants.FilePathConstants;
 import clap.server.common.utils.FileUtils;
 import clap.server.domain.model.member.Member;
 import clap.server.exception.ApplicationException;
-import clap.server.exception.code.AttachmentErrorcode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,20 +21,11 @@ import java.io.IOException;
 class UpdateMemberInfoService implements UpdateMemberInfoUsecase {
     private final MemberService memberService;
     private final S3UploadPort s3UploadPort;
-    private final CommandMemberPort commandMemberPort;
 
     @Override
     public void updateMemberInfo(Long memberId, UpdateMemberInfoRequest request, MultipartFile profileImage) throws IOException {
         Member member = memberService.findActiveMember(memberId);
-        String profileImageUrl = null;
-        if (!profileImage.isEmpty()){
-            if(!FileUtils.validImageFile(profileImage.getInputStream())) {
-                throw new ApplicationException(AttachmentErrorcode.UNSUPPORTED_FILE_TYPE);
-            }
-            profileImageUrl = s3UploadPort.uploadSingleFile(FilePathConstants.MEMBER_IMAGE, profileImage);
-        }
         member.updateMemberInfo(request.name(), request.agitNotification(), request.emailNotification(),
                 request.kakaoWorkNotification(), profileImageUrl);
-        commandMemberPort.save(member);
     }
 }
--- src/main/java/clap/server/domain/model/member/Member.java
+++ src/main/java/clap/server/domain/model/member/Member.java
@@ -66,8 +66,4 @@ public class Member extends BaseTime {
     public boolean isReviewer() {
         return this.memberInfo != null && this.memberInfo.isReviewer();
     }
-
-    public void setStatusDeleted() {
-        this.status = MemberStatus.DELETED;
-    }
 }
