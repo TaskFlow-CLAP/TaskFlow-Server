750e89a94a4054949f10a458b6b927f2e2355fd1
joowojr <joowojr@gmail.com>
2025-01-30 14:45:08 +0700

    CLAP-172 Feat: 회원 정보 수정 API 구현
    <footer>
    - 관련: #164

--- src/main/java/clap/server/adapter/inbound/web/member/MemberInfoController.java
+++ src/main/java/clap/server/adapter/inbound/web/member/MemberInfoController.java
@@ -11,12 +11,16 @@ import clap.server.common.annotation.architecture.WebAdapter;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Tag(name = "01. Member", description = "회원 정보 조회 및 수정 API")
 @WebAdapter
@@ -33,9 +37,18 @@ public class MemberInfoController {
         return ResponseEntity.ok(memberProfileUsecase.getMemberProfile(userInfo.getUserId()));
     }
 
-    @Operation(summary = "회원 프로필 조회 API")
+    @Operation(summary = "회원 상세 정보 조회 API")
     @GetMapping("/info")
     public ResponseEntity<MemberDetailInfoResponse> getMemberDetailInfo(@AuthenticationPrincipal SecurityUserDetails userInfo) {
         return ResponseEntity.ok(memberDetailInfoUsecase.getMemberInfo(userInfo.getUserId()));
+    }
+
+    @Operation(summary = "회원 정보 수정 API", description = "이미지 수정이 없을 시에는 profileImage를 보내지 않습니다.")
+    @PatchMapping(value = "/info", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public void updateMemberDetailInfo(
+            @RequestPart(name = "memberInfo") UpdateMemberInfoRequest request,
+            @RequestPart(name = "profileImage", required = false) MultipartFile profileImage,
+            @AuthenticationPrincipal SecurityUserDetails userInfo) throws IOException {
+        updateMemberInfoUsecase.updateMemberInfo(userInfo.getUserId(), request, profileImage);
     }
 }
