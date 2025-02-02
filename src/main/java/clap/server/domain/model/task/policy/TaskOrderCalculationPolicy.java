dd55e7a0964daa631b9bc0473e94bee0fc602bfe
joowojr <joowojr@gmail.com>
2025-02-02 16:37:45 +0900

    CLAP-213 Feat: Task 도메인 클래스에서 비즈니스 정책을 캡슐화하여 policy 클래스로 분리
    <footer>
    - 관련: #215

---
+++ src/main/java/clap/server/domain/model/task/policy/TaskOrderCalculationPolicy.java
@@ -0,0 +1,45 @@
+package clap.server.domain.model.task.policy;
+
+import clap.server.common.annotation.architecture.Policy;
+import clap.server.domain.model.task.Task;
+import clap.server.exception.DomainException;
+import clap.server.exception.code.TaskErrorCode;
+import lombok.RequiredArgsConstructor;
+
+import static clap.server.domain.model.task.constants.TaskProcessorOrderPolicy.DEFAULT_PROCESSOR_ORDER_GAP;
+
+@RequiredArgsConstructor
+@Policy
+public class TaskOrderCalculationPolicy {
+
+    public long calculateOrderForTop(Task prevTask, Task nextTask) {
+        Long prevTaskOrder = prevTask == null ? null : prevTask.getProcessorOrder();
+        if (prevTaskOrder == null){
+            return nextTask.getProcessorOrder() - DEFAULT_PROCESSOR_ORDER_GAP;
+        }
+        return calculateNewProcessorOrder(prevTaskOrder, nextTask.getProcessorOrder());
+    }
+
+    public long calculateOrderForBottom(Task prevTask, Task nextTask) {
+        Long nextTaskOrder = nextTask == null ? null : nextTask.getProcessorOrder();
+        if (nextTaskOrder == null){
+            return prevTask.getProcessorOrder() + DEFAULT_PROCESSOR_ORDER_GAP;
+        }
+        return calculateNewProcessorOrder(prevTask.getProcessorOrder(), nextTaskOrder);
+    }
+
+    public long calculateNewProcessorOrder(Long prevTaskOrder, Long nextTaskOrder) {
+        if (prevTaskOrder != null && nextTaskOrder != null) {
+            if (nextTaskOrder - prevTaskOrder < 2) {
+                throw new DomainException(TaskErrorCode.INVALID_TASK_ORDER);
+            }
+            return (prevTaskOrder + nextTaskOrder) / 2;
+        }
+        // 기본값 (예외적인 상황 방지)
+        else {
+            return DEFAULT_PROCESSOR_ORDER_GAP;
+        }
+    }
+
+}
+
