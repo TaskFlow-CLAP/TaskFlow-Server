98552f90b84bbc2d7e0ecdd7032272b7553d6ae6
andrew <leegd120@gmail.com>
2025-02-02 20:07:05 +0900

    CLAP-220 Fix: 처리자 변경 관련 아키텍처 구조에 맞게 전체 수정

--- src/main/java/clap/server/adapter/inbound/web/dto/task/UpdateTaskProcessorRequest.java
+++ src/main/java/clap/server/adapter/inbound/web/dto/task/UpdateTaskProcessorRequest.java
@@ -1,7 +1,13 @@
 package clap.server.adapter.inbound.web.dto.task;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
 public record UpdateTaskProcessorRequest(
+
+        @Schema(description = "변경할 담당자 고유 ID", example = "1")
+        @NotBlank
         Long processorId
 ) {
 }
