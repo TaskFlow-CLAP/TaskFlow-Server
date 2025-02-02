22b939f94c0078e82d3bb57a857fb0f8386a8c5a
parkjaehak <atom8426@gmail.com>
2025-02-02 12:08:54 +0900

    CLAP-209 Refactor: 서비스별 스웨거 구분

--- src/main/java/clap/server/adapter/inbound/web/comment/PostCommentController.java
+++ src/main/java/clap/server/adapter/inbound/web/comment/PostCommentController.java
@@ -17,7 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-@Tag(name = "02. Task", description = "작업 생성/수정 API")
+@Tag(name = "02. Task [생성/수정]", description = "작업 생성/수정 API")
 @WebAdapter
 @RestController
 @RequiredArgsConstructor
