4f338adf73c571e1bbb597f9830bbc4591b90f9f
hyoseong-Choi <gytjd9516@naver.com>
2025-02-02 18:44:40 +0900

    CLAP-214 Fix : addConverter 수정
    <footer>
    - 관련: #216

--- src/main/java/clap/server/config/web/WebConfig.java
+++ src/main/java/clap/server/config/web/WebConfig.java
@@ -2,22 +2,17 @@ package clap.server.config.web;
 
 import clap.server.common.utils.StringToPeriodTypeConverter;
 import clap.server.common.utils.StringToStatisticsTypeConverter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ConversionServiceFactoryBean;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Set;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Bean
-    public ConversionServiceFactoryBean conversionService() {
-        ConversionServiceFactoryBean conversionService = new ConversionServiceFactoryBean();
-        conversionService.setConverters(Set.of(
-                new StringToStatisticsTypeConverter(),
-                new StringToPeriodTypeConverter()
-        ));
-        return conversionService;
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToStatisticsTypeConverter());
+        registry.addConverter(new StringToPeriodTypeConverter());
     }
 }
