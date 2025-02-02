dc2cba859dedfce29b5dd312445dbe23a4709852
Sihun23 <gcsw16@Gachonui-MacBookPro-2.local>
2025-02-02 16:21:44 +0900

    CLAP-215 fix:시간 정렬 파라미터 추가

--- src/main/java/clap/server/adapter/inbound/web/log/LogController.java
+++ src/main/java/clap/server/adapter/inbound/web/log/LogController.java
@@ -10,15 +10,13 @@ import clap.server.common.annotation.architecture.WebAdapter;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Tag(name = "05. Admin")
 @WebAdapter
@@ -35,9 +33,15 @@ public class LogController {
     public ResponseEntity<PageResponse<AnonymousLogResponse>> getLoginAttempts(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "20") int pageSize,
+            @RequestParam(defaultValue = "timestamp") String sortBy,
+            @RequestParam(defaultValue = "desc") String sortDirection,
             @ModelAttribute FilterLogRequest anonymousLogRequest,
             @AuthenticationPrincipal SecurityUserDetails userInfo) {
-        Pageable pageable = PageRequest.of(page, pageSize);
+        Pageable pageable = PageRequest.of(
+                page,
+                pageSize,
+                "asc".equalsIgnoreCase(sortDirection) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending()
+        );
         return ResponseEntity.ok(findApiLogsUsecase.filterAnonymousLogs(anonymousLogRequest, pageable));
     }
 
@@ -47,9 +51,15 @@ public class LogController {
     public ResponseEntity<PageResponse<MemberLogResponse>> getApiCalls(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "20") int pageSize,
+            @RequestParam(defaultValue = "timestamp") String sortBy,
+            @RequestParam(defaultValue = "desc") String sortDirection,
             @ModelAttribute FilterLogRequest memberLogRequest,
             @AuthenticationPrincipal SecurityUserDetails userInfo) {
-        Pageable pageable = PageRequest.of(page, pageSize);
+        Pageable pageable = PageRequest.of(
+                page,
+                pageSize,
+                "asc".equalsIgnoreCase(sortDirection) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending()
+        );
         return ResponseEntity.ok(findApiLogsUsecase.filterMemberLogs(memberLogRequest, pageable));
     }
 }
