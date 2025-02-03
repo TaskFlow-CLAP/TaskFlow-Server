c08b5b783d600085445a433a5f4fac2ef378c303
joowojr <joowojr@gmail.com>
2025-02-04 00:38:58 +0900

    CLAP-251 Fix: 잘못된 커밋 수정

--- src/main/java/clap/server/adapter/inbound/web/dto/task/response/TaskBoardResponse.java
+++ src/main/java/clap/server/adapter/inbound/web/dto/task/response/TaskBoardResponse.java
@@ -5,6 +5,9 @@ import java.util.List;
 public record TaskBoardResponse(
         List<TaskItemResponse> tasksInProgress,
         List<TaskItemResponse> tasksPendingComplete,
-        List<TaskItemResponse> tasksCompleted
+        List<TaskItemResponse> tasksCompleted,
+        boolean hasNext,
+        boolean isFirst,
+        boolean isLast
 ){
 }
