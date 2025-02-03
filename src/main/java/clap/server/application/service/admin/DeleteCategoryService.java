d787a1d421a18dd0d247a28d5f1bc9035a155db7
Choi hyoseong <105284587+hyoseong-Choi@users.noreply.github.com>
2025-02-03 11:58:12 +0900

    CLAP-214 Hotfix : DeleteCategoryService 카테고리 삭제 로직 수정
    * CLAP-110 Feature : 담당자별 작업 처리량 조회 API 구현
    
    <footer>
    - 관련: #73
    
    * CLAP-111 Refactor : 통계 조회 API 리팩토링 및 기능 수정
    
    <footer>
    - 관련: #74
    
    * 내 작업한 내용에 대한 설명
    
    * 담당자 조회 API 구현
    
    * CLAP-111 Refactor : 통계 조회 API 주소 통합, 리팩토링, 예외처리
    
    <footer>
    - 관련: #74
    
    * Bug : 프로퍼티파일 수정
    
    * CLAP-104 CI/CD : CI에서 s3.yml 파일 생성하도록 수정
    
    * CLAP-146 Feature : 카테고리 목록 조회 API 구현
    
    <footer>
    - 관련: #118
    
    * CLAP-147 Feature : 카테고리 수정 API 구현
    
    <footer>
    - 관련: #119
    
    * CLAP-147 Feature : 카테고리 추가, 수정 API 리뷰반영 수정
    
    <footer>
    - 관련: #119
    
    * CLAP-148 Feature : 카테고리 삭제 API 구현
    
    <footer>
    - 관련: #120
    
    * CLAP-148 Feature : 카테고리 삭제 API 수정
    
    <footer>
    - 관련: #120
    
    * CLAP-148 Docs : 카테고리 API 스웨거 수정
    
    <footer>
    - 관련: #120
    
    * CLAP-107 Bug : CI test yml파일 key 중복 수정
    
    <footer>
    - 관련: #60
    
    * CLAP-148 Feature : 카테고리 CUD 리뷰 반영 수정, 조회 반환 양식 수정
    
    <footer>
    - 관련: #120
    
    * CLAP-214 Cleanup : 통계, 카테고리 미흡한부분 리팩토링
    
    <footer>
    - 관련: #216
    
    * CLAP-214 Fix : 통계조회 API 파라미터 바인딩 오류 수정
    
    <footer>
    - 관련: #216
    
    * CLAP-214 Fix : addConverter 수정
    
    <footer>
    - 관련: #216
    
    * CLAP-214 Hotfix : 추가한 파일들 제거
    
    <footer>
    - 관련: #216
    
    * CLAP-214 Hotfix : FindStatisticsController 스위치문 수정
    
    <footer>
    - 관련: #216
    
    * CLAP-214 Hotfix : AddCategoryService 지연로딩 오류 수정
    
    <footer>
    - 관련: #216
    
    * CLAP-214 Hotfix : DeleteCategoryService 카테고리 삭제 로직 수정
    
    <footer>
    - 관련: #216

--- src/main/java/clap/server/application/service/admin/DeleteCategoryService.java
+++ src/main/java/clap/server/application/service/admin/DeleteCategoryService.java
@@ -2,9 +2,11 @@ package clap.server.application.service.admin;
 
 import clap.server.application.port.inbound.admin.DeleteCategoryUsecase;
 import clap.server.application.port.inbound.domain.MemberService;
+import clap.server.application.port.outbound.task.CommandCategoryPort;
 import clap.server.application.port.outbound.task.LoadCategoryPort;
 import clap.server.common.annotation.architecture.ApplicationService;
 import clap.server.domain.model.member.Member;
+import clap.server.domain.model.task.Category;
 import clap.server.exception.ApplicationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,13 +18,15 @@ import static clap.server.exception.code.TaskErrorCode.CATEGORY_NOT_FOUND;
 public class DeleteCategoryService implements DeleteCategoryUsecase {
     private final LoadCategoryPort loadCategoryPort;
     private final MemberService memberService;
+    private final CommandCategoryPort commandCategoryPort;
 
     @Override
     @Transactional
     public void deleteCategory(Long adminId, Long categoryId) {
         Member admin = memberService.findActiveMember(adminId);
-        loadCategoryPort.findById(categoryId)
-                .orElseThrow(() -> new ApplicationException(CATEGORY_NOT_FOUND))
-                .deleteCategory(admin);
+        Category category = loadCategoryPort.findById(categoryId)
+                .orElseThrow(() -> new ApplicationException(CATEGORY_NOT_FOUND));
+        category.deleteCategory(admin);
+        commandCategoryPort.save(category);
     }
 }
