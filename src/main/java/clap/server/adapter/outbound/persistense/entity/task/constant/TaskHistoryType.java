016a26935acdb76b84941d8563a5b7408f179285
joowojr <joowojr@gmail.com>
2025-02-03 21:15:34 +0900

    CLAP-245 Feat: 작업 거부 API 구현
    <footer>
    - 관련: #260

--- src/main/java/clap/server/adapter/outbound/persistense/entity/task/constant/TaskHistoryType.java
+++ src/main/java/clap/server/adapter/outbound/persistense/entity/task/constant/TaskHistoryType.java
@@ -10,7 +10,8 @@ public enum TaskHistoryType {
     COMMENT_FILE("댓글 첨부파일"),
     STATUS_SWITCHED("상태 전환"),
     PROCESSOR_ASSIGNED("처리자 할당"),
-    PROCESSOR_CHANGED("처리자 변경");
+    PROCESSOR_CHANGED("처리자 변경"),
+    TASK_TERMINATED("작업 종료됨");
 
     private final String description;
 }
