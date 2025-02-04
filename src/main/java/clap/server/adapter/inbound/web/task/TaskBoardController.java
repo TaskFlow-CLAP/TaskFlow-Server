cb7794164a9eb0de567a68d2a1db4322e247e1b2
joowojr <joowojr@gmail.com>
2025-02-04 10:05:13 +0900

    CLAP-239 Feat: 카테고리 필터를 배열로 변경
    <footer>
    - 관련: #254

--- src/main/java/clap/server/adapter/inbound/web/task/TaskBoardController.java
+++ src/main/java/clap/server/adapter/inbound/web/task/TaskBoardController.java
@@ -6,8 +6,8 @@ import clap.server.adapter.inbound.web.dto.task.request.UpdateTaskOrderRequest;
 import clap.server.adapter.inbound.web.dto.task.response.TaskBoardResponse;
 import clap.server.adapter.outbound.persistense.entity.task.constant.TaskStatus;
 import clap.server.application.port.inbound.task.FilterTaskBoardUsecase;
-import clap.server.application.port.inbound.task.UpdateTaskBoardUsecase;
 import clap.server.application.port.inbound.task.GetTaskBoardUsecase;
+import clap.server.application.port.inbound.task.UpdateTaskBoardUsecase;
 import clap.server.application.port.inbound.task.UpdateTaskOrderAndStatusUsecase;
 import clap.server.common.annotation.architecture.WebAdapter;
 import io.swagger.v3.oas.annotations.Operation;
@@ -15,9 +15,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
@@ -26,7 +23,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
-@Slf4j
 @Tag(name = "02. Task [담당자]")
 @WebAdapter
 @RestController
@@ -41,23 +37,22 @@ public class TaskBoardController {
     @Operation(summary = "작업 보드 조회 API")
     @Secured({"ROLE_MANAGER"})
     @GetMapping
-    public ResponseEntity<TaskBoardResponse> getTaskBoard(@RequestParam(defaultValue = "0") int page,
-                                                          @RequestParam(defaultValue = "20") int pageSize,
+    public ResponseEntity<TaskBoardResponse> getTaskBoard(
                                                           @Parameter(description = "작업 완료 일자 조회 기준, yyyy-mm-dd 형식으로 입력합니다.") @RequestParam(required = false)
-                                                          @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate untilDate,
-                                                          @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "필터링 조회 request") @ModelAttribute FilterTaskBoardRequest request,
+                                                          @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate fromDate,
+                                                          @ModelAttribute FilterTaskBoardRequest request,
                                                           @AuthenticationPrincipal SecurityUserDetails userInfo) {
-        Pageable pageable = PageRequest.of(page, pageSize);
         if (request != null) {
-            return ResponseEntity.ok(filterTaskBoardUsecase.getTaskBoardByFilter(userInfo.getUserId(), untilDate, request, pageable));
+            return ResponseEntity.ok(filterTaskBoardUsecase.getTaskBoardByFilter(userInfo.getUserId(), fromDate, request));
         } else
-            return ResponseEntity.ok(getTaskBoardUsecase.getTaskBoards(userInfo.getUserId(), untilDate, pageable));
+            return ResponseEntity.ok(getTaskBoardUsecase.getTaskBoards(userInfo.getUserId(), fromDate));
     }
 
     @Operation(summary = "작업 보드 순서 및 상태 변경 API")
     @Secured({"ROLE_MANAGER"})
     @PatchMapping
-    public void updateTaskBoard(@Parameter(description = "전환될 작업의 상태, 상태 전환이 아니라면 입력 X", schema = @Schema(allowableValues = {"IN_PROGRESS", "PENDING_COMPLETED", "COMPLETED"}))
+    public void updateTaskBoard(@Parameter(description = "전환될 작업의 상태, 상태 전환이 아니라면 입력 X",
+            schema = @Schema(allowableValues = {"IN_PROGRESS", "PENDING_COMPLETED", "COMPLETED"}))
                                 @RequestParam(required = false) TaskStatus status,
                                 @RequestBody UpdateTaskOrderRequest request,
                                 @AuthenticationPrincipal SecurityUserDetails userInfo) {
