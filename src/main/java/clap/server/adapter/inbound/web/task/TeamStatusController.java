372a65ef0c09f1f4630486d2e0e370f26746b7f4
nano-mm <nano123@gachon.ac.kr>
2025-02-04 23:38:47 +0900

    CLAP-256 Docs: 스웨거 명세 추가

--- src/main/java/clap/server/adapter/inbound/web/task/TeamStatusController.java
+++ src/main/java/clap/server/adapter/inbound/web/task/TeamStatusController.java
@@ -5,7 +5,7 @@ import clap.server.adapter.inbound.web.dto.task.request.FilterTeamStatusRequest;
 import clap.server.adapter.inbound.web.dto.task.response.TeamStatusResponse;
 import clap.server.application.service.task.TeamStatusService;
 import clap.server.common.annotation.architecture.WebAdapter;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -14,12 +14,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/team-status")
 @RequiredArgsConstructor
-@Tag(name = "팀 현황 조회 API")
 @WebAdapter
 public class TeamStatusController {
-
+    
     private final TeamStatusService teamStatusService;
-
+    @Operation(summary = "팀 현황 필터링 조회 API")
     @GetMapping("/filter")
     public ResponseEntity<TeamStatusResponse> filterTeamStatus(@Validated @ModelAttribute FilterTeamStatusRequest filter) {
         TeamStatusResponse response = teamStatusService.filterTeamStatus(filter);
