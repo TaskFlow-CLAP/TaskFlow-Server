00100e303691c96a2946a93596d211869ca384c2
parkjaehak <atom8426@gmail.com>
2025-02-04 19:50:02 +0900

    CLAP-221 Fix: 상태 불일치 에러 케이스 테스트

--- src/test/java/clap/server/application/service/task/ApprovalTaskServiceTest.java
+++ src/test/java/clap/server/application/service/task/ApprovalTaskServiceTest.java
@@ -16,6 +16,8 @@ import clap.server.domain.model.task.Label;
 import clap.server.domain.model.task.Task;
 import clap.server.domain.model.task.TaskHistory;
 import clap.server.domain.policy.task.RequestedTaskUpdatePolicy;
+import clap.server.exception.DomainException;
+import clap.server.exception.code.TaskErrorCode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,6 +27,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
 
@@ -45,7 +48,6 @@ class ApprovalTaskServiceTest {
 
     @Mock
     private LabelService labelService;
-
     @Mock
     private RequestedTaskUpdatePolicy requestedTaskUpdatePolicy;
 
@@ -66,7 +68,7 @@ class ApprovalTaskServiceTest {
         processor = TestDataFactory.createManager();
         mainCategory = TestDataFactory.createMainCategory();
         category = TestDataFactory.createCategory(mainCategory);
-        task = TestDataFactory.createTask( 1L,"TC001", "제목1", TaskStatus.REQUESTED, category, null, processor);
+        task = TestDataFactory.createTask(1L, "TC001", "제목1", TaskStatus.REQUESTED, category, null, processor);
     }
 
     @Test
@@ -88,10 +90,27 @@ class ApprovalTaskServiceTest {
 
         //then
         assertThat(response).isNotNull();
-        assertThat(response.taskStatus()).isEqualTo(TaskStatus.IN_PROGRESS);
         assertThat(response.taskId()).isEqualTo(task.getTaskId());
+        assertThat(response.taskStatus()).isEqualTo(TaskStatus.IN_PROGRESS);
         verify(requestedTaskUpdatePolicy).validateTaskRequested(task);
-        verify(commandTaskHistoryPort, times(1)).save(any(TaskHistory.class));
+    }
+
+    @Test
+    @DisplayName("작업 승인 처리 중 예외 - 상태 불일치")
+    void approvalTask_throwsDomainException_whenTaskStatusIsNotRequested() {
+        //given
+        Long reviewerId = 2L;
+        Long taskId = 1L;
+        ApprovalTaskRequest approvalTaskRequest = new ApprovalTaskRequest(2L, 2L, null, null);
+        task = TestDataFactory.createTask(1L, "TC001", "제목1", TaskStatus.COMPLETED, category, null, processor);
+        when(taskService.findById(taskId)).thenReturn(task);
+
+        //when
+        doThrow(new DomainException(TaskErrorCode.TASK_STATUS_MISMATCH))
+                .when(requestedTaskUpdatePolicy).validateTaskRequested(task);
+        //then
+        assertThatThrownBy(() -> approvalTaskService.approvalTaskByReviewer(reviewerId, taskId, approvalTaskRequest))
+                .isInstanceOf(DomainException.class)
+                .hasMessage(TaskErrorCode.TASK_STATUS_MISMATCH.getMessage());
     }
 }
-
