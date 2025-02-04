fefdfc992bcffe44ee10ca3504e94f813ba334d1
joowojr <joowojr@gmail.com>
2025-02-04 23:05:46 +0900

    CLAP-269 Refactor: 회원 저장 시에 일괄로 저장되도록 수정
    <footer>
    - 관련: #306

--- src/main/java/clap/server/application/port/inbound/admin/RegisterMemberCSVUsecase.java
+++ src/main/java/clap/server/application/port/inbound/admin/RegisterMemberCSVUsecase.java
@@ -2,6 +2,8 @@ package clap.server.application.port.inbound.admin;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 public interface RegisterMemberCSVUsecase {
-    int registerMembersFromCsv(Long adminId, MultipartFile file);
+    int registerMembersFromCsv(Long adminId, MultipartFile file) throws IOException;
 }
--- src/main/java/clap/server/application/service/admin/CsvParseService.java
+++ src/main/java/clap/server/application/service/admin/CsvParseService.java
@@ -1,16 +1,16 @@
 package clap.server.application.service.admin;
 
+import clap.server.adapter.outbound.persistense.entity.member.constant.MemberRole;
 import clap.server.application.port.outbound.member.LoadDepartmentPort;
-import clap.server.common.annotation.architecture.ApplicationService;
 import clap.server.domain.model.member.Department;
 import clap.server.domain.model.member.Member;
 import clap.server.domain.model.member.MemberInfo;
-import clap.server.adapter.outbound.persistense.entity.member.constant.MemberRole;
 import clap.server.exception.ApplicationException;
 import clap.server.exception.code.DepartmentErrorCode;
 import clap.server.exception.code.MemberErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedReader;
@@ -24,38 +24,41 @@ import static clap.server.domain.model.member.MemberInfo.toMemberInfo;
 
 
 @Slf4j
-@ApplicationService
+@Service
 @RequiredArgsConstructor
 public class CsvParseService {
 
     private final LoadDepartmentPort loadDepartmentPort;
 
-    public List<Member> parse(MultipartFile file) {
+    public List<Member> parseDataAndMapToMember(MultipartFile file) {
         List<Member> members = new ArrayList<>();
+        List<Department> departments = loadDepartmentPort.findActiveDepartments();
+
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
             // 첫 번째 줄은 헤더로 간주하고 다음 줄부터 파싱
             String headerLine = reader.readLine();
             if (headerLine == null) {
-                throw ApplicationException.from(MemberErrorCode.INVALID_CSV_FORMAT);
+                throw new ApplicationException(MemberErrorCode.INVALID_CSV_FORMAT);
             }
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] fields = line.split(",");
                 if (fields.length != 7) {
-                    throw ApplicationException.from(MemberErrorCode.INVALID_CSV_FORMAT);
+                    throw new ApplicationException(MemberErrorCode.INVALID_CSV_FORMAT);
                 }
-                members.add(mapToMember(fields));
+                members.add(mapToMember(fields, departments));
             }
         } catch (IOException e) {
-            throw ApplicationException.from(MemberErrorCode.CSV_PARSING_ERROR);
+            throw new ApplicationException(MemberErrorCode.CSV_PARSING_ERROR);
         }
         return members;
     }
 
-    private Member mapToMember(String[] fields) {
-        // 부서 ID로 Department 객체 조회
+    private Member mapToMember(String[] fields, List<Department> departments) {
         Long departmentId = Long.parseLong(fields[2].trim());
-        Department department = loadDepartmentPort.findById(departmentId)
+        Department department = departments.stream()
+                .filter(dept -> dept.getDepartmentId().equals(departmentId))
+                .findFirst()
                 .orElseThrow(() -> new ApplicationException(DepartmentErrorCode.DEPARTMENT_NOT_FOUND));
 
         MemberInfo memberInfo = toMemberInfo(
