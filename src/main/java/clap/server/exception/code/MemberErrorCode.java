969000751aa06a3777030b23c79ed5bd88b5d445
joowojr <joowojr@gmail.com>
2025-02-05 13:08:47 +0900

    CLAP-275 Refactor: 에러코드 분리
    <footer>
    관련 : #318

--- src/main/java/clap/server/exception/code/MemberErrorCode.java
+++ src/main/java/clap/server/exception/code/MemberErrorCode.java
@@ -15,7 +15,8 @@ public enum MemberErrorCode implements BaseErrorCode {
     INVALID_CSV_FORMAT(HttpStatus.BAD_REQUEST, "MEMBER_007", "CSV 파일 형식이 잘못되었습니다."),
     CSV_PARSING_ERROR(HttpStatus.BAD_REQUEST, "MEMBER_008", "CSV 데이터 파싱 중 오류가 발생했습니다."),
     MEMBER_REGISTRATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "MEMBER_009", "회원 등록 중 오류가 발생했습니다."),
-    NAME_CANNOT_BE_EMPTY(HttpStatus.BAD_REQUEST, "MEMBER_010", "이름은 공백일 수 없습니다.")
+    NAME_CANNOT_BE_EMPTY(HttpStatus.BAD_REQUEST, "MEMBER_010", "이름은 공백일 수 없습니다."),
+    MANAGER_ONLY_REVIEW(HttpStatus.INTERNAL_SERVER_ERROR, "MEMBER_011", "담당자만 리뷰 권한이 있습니."),
 
     ;
 
