30104054e25e5a1597b77c4bff68c4f1049b4bea
joowojr <joowojr@gmail.com>
2025-02-06 12:35:03 +0900

    CLAP-294 Feat: 비밀번호 재설정 이메일 전송 및 인증번호 검증 API 구현
    <footer>
    - 관련: #346

--- src/main/java/clap/server/application/port/inbound/member/VerifyEmailCodeUsecase.java
+++ src/main/java/clap/server/application/port/inbound/member/VerifyEmailCodeUsecase.java
@@ -1,5 +1,5 @@
 package clap.server.application.port.inbound.member;
 
 public interface VerifyEmailCodeUsecase {
-    void verifyEmailCode(Long memberId);
+    void verifyEmailCode(Long memberId, String code);
 }
