e2c2eebf37fcc9522eb939645bcbf01fdbad936c
joowojr <joowojr@gmail.com>
2025-02-06 11:23:19 +0900

    CLAP-282 Fix: 비밀번호 검증 로직 수정
    <footer>
    - 관련: #346

--- src/main/java/clap/server/application/port/inbound/member/VerifyPasswordUseCase.java
+++ src/main/java/clap/server/application/port/inbound/member/VerifyPasswordUseCase.java
@@ -1,5 +1,5 @@
 package clap.server.application.port.inbound.member;
 
 public interface VerifyPasswordUseCase {
-    void verifyPassword(Long memberId, String password);
+    void verifyPassword(Long memberId, String inputPassword);
 }
--- src/main/java/clap/server/application/service/member/VerifyPasswordService.java
+++ src/main/java/clap/server/application/service/member/VerifyPasswordService.java
@@ -4,10 +4,16 @@ import clap.server.application.port.inbound.domain.MemberService;
 import clap.server.application.port.inbound.member.VerifyPasswordUseCase;
 import clap.server.common.annotation.architecture.ApplicationService;
 import clap.server.domain.model.member.Member;
+import clap.server.exception.ApplicationException;
+import clap.server.exception.AuthException;
+import clap.server.exception.code.AuthErrorCode;
+import clap.server.exception.code.MemberErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @ApplicationService
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -16,9 +22,10 @@ class VerifyPasswordService implements VerifyPasswordUseCase {
     private final PasswordEncoder passwordEncoder;
 
     @Override
-    public void verifyPassword(Long memberId, String password) {
+    public void verifyPassword(Long memberId, String inputPassword) {
         Member member = memberService.findActiveMember(memberId);
-        String encodedPassword = passwordEncoder.encode(password);
-        member.verifyPassword(encodedPassword);
+        if (!passwordEncoder.matches(member.getPassword(), inputPassword)) {
+            throw new ApplicationException(MemberErrorCode.PASSWORD_VERIFY_FAILED);
+        }
     }
 }
