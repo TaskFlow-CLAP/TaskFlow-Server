de45def3b3d5e24cd79dfb6e1614a71b8a627a01
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-02-06 10:16:51 +0900

    Update PostCommentService.java

--- src/main/java/clap/server/application/service/history/PostCommentService.java
+++ src/main/java/clap/server/application/service/history/PostCommentService.java
@@ -42,7 +42,7 @@ public class PostCommentService implements SaveCommentUsecase, SaveCommentAttach
         Task task = taskService.findById(taskId);
         Member member = memberService.findActiveMember(userId);
 
-        // ROLE_USER일 경우 => 요청자인지 확인
+        // 일반 회원일 경우 => 요청자인지 확인
         if (Member.checkCommenter(task, member)) {
             Comment comment = Comment.createComment(member, task, request.content());
             Comment savedComment = commandCommentPort.saveComment(comment);
