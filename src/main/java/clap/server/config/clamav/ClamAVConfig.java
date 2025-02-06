207275d17cea1088a5287f747b137b179b2b24ce
joowojr <joowojr@gmail.com>
2025-02-07 00:31:04 +0900

    CLAP-283 Chore: ClamAVConfig 추가
    <footer>
    - 관련: #328

---
+++ src/main/java/clap/server/config/clamav/ClamAVConfig.java
@@ -0,0 +1,20 @@
+package clap.server.config.clamav;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import xyz.capybara.clamav.ClamavClient;
+
+@Configuration
+public class ClamAVConfig {
+    @Value("${clamav.host}")
+    private String host;
+    
+    @Value("${clamav.port}")
+    private int port;
+    
+    @Bean
+    public ClamavClient clamavClient() {
+        return new ClamavClient(host, port);
+    }
+}
