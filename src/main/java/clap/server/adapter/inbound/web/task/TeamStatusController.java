8aa18d207db6630201ad6a148402b44495f9167d
hyoseong-Choi <gytjd9516@naver.com>
2025-02-06 15:47:04 +0900

    CLAP-256 Fix : TeamStatusService Secured 추가
    <footer>
    - 관련: #281

--- src/main/java/clap/server/adapter/inbound/web/task/TeamStatusController.java
+++ src/main/java/clap/server/adapter/inbound/web/task/TeamStatusController.java
@@ -1,7 +1,6 @@
 package clap.server.adapter.inbound.web.task;
 
 import clap.server.adapter.inbound.web.dto.task.request.FilterTeamStatusRequest;
-
 import clap.server.adapter.inbound.web.dto.task.response.TeamStatusResponse;
 import clap.server.application.service.task.TeamStatusService;
 import clap.server.common.annotation.architecture.WebAdapter;
@@ -9,8 +8,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -23,6 +24,7 @@ public class TeamStatusController {
     private final TeamStatusService teamStatusService;
     @Operation(summary = "팀 현황 필터링 조회 API")
     @GetMapping("/filter")
+    @Secured("ROLE_MANAGER")
     public ResponseEntity<TeamStatusResponse> filterTeamStatus(@ModelAttribute FilterTeamStatusRequest filter) {
         TeamStatusResponse response = teamStatusService.filterTeamStatus(filter);
         return ResponseEntity.ok(response != null ? response : new TeamStatusResponse(List.of(), 0, 0, 0));
