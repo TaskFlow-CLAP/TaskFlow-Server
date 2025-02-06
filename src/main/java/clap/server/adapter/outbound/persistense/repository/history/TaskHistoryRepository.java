797de42b58fb545e2d007a34b261c3343bae2952
parkjaehak <atom8426@gmail.com>
2025-02-06 09:37:55 +0900

    CLAP-291 HotFix: 댓글 삭제 시 히스토리 및 첨부파일 삭제 처리

--- src/main/java/clap/server/adapter/outbound/persistense/repository/history/TaskHistoryRepository.java
+++ src/main/java/clap/server/adapter/outbound/persistense/repository/history/TaskHistoryRepository.java
@@ -1,12 +1,12 @@
-package clap.server.adapter.outbound.persistense.repository.task;
+package clap.server.adapter.outbound.persistense.repository.history;
 
 import clap.server.adapter.outbound.persistense.entity.task.TaskHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface TaskHistoryRepository extends JpaRepository<TaskHistoryEntity, Long> , TaskHistoryCustomRepository{
-    List<TaskHistoryEntity> findAllByTaskModificationInfo_Task_TaskId(Long taskId);
 }
