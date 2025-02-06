e98b5bc17e23d45a9046a2cd8a44ff7acc209e95
joowojr <joowojr@gmail.com>
2025-02-07 01:12:01 +0900

    CLAP-310 Refactor: 인증번호 전송 request body 추가
    <footer>
    - 관련: #380

--- src/main/java/clap/server/adapter/inbound/web/member/EmailVerificationController.java
+++ src/main/java/clap/server/adapter/inbound/web/member/EmailVerificationController.java
@@ -1,6 +1,7 @@
 package clap.server.adapter.inbound.web.member;
 
 import clap.server.adapter.inbound.security.service.SecurityUserDetails;
+import clap.server.adapter.inbound.web.dto.member.response.SendVerificationCodeRequest;
 import clap.server.application.port.inbound.member.SendVerificationEmailUsecase;
 import clap.server.application.port.inbound.member.VerifyEmailCodeUsecase;
 import clap.server.common.annotation.architecture.WebAdapter;
@@ -9,27 +10,28 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "00. Auth [인증번호]")
 @WebAdapter
 @RequiredArgsConstructor
-@RequestMapping("/api/members")
+@RequestMapping("/api")
 public class EmailVerificationController {
     private final SendVerificationEmailUsecase sendVerificationEmailUsecase;
     private final VerifyEmailCodeUsecase verifyEmailCodeUsecase;
 
     @Operation(summary = "인증번호 전송 API")
     @PostMapping("/verification/email")
-    public void sendVerificationEmail(@AuthenticationPrincipal SecurityUserDetails userInfo){
-        sendVerificationEmailUsecase.sendVerificationCode(userInfo.getUserId());
+    public void sendVerificationEmail(@RequestBody SendVerificationCodeRequest request) {
+        sendVerificationEmailUsecase.sendVerificationCode(request);
     }
 
     @Operation(summary = "인증번호 검증 API")
     @PostMapping("/verification")
     public void sendVerificationEmail(@AuthenticationPrincipal SecurityUserDetails userInfo,
-                                      @RequestParam String code){
+                                      @RequestParam String code) {
         verifyEmailCodeUsecase.verifyEmailCode(userInfo.getUserId(), code);
     }
 }
--- src/main/java/clap/server/application/service/auth/EmailVerificationService.java
+++ src/main/java/clap/server/application/service/auth/EmailVerificationService.java
@@ -1,32 +1,37 @@
 package clap.server.application.service.auth;
 
+import clap.server.adapter.inbound.web.dto.member.response.SendVerificationCodeRequest;
 import clap.server.application.port.inbound.domain.MemberService;
 import clap.server.application.port.inbound.member.SendVerificationEmailUsecase;
 import clap.server.application.port.inbound.member.VerifyEmailCodeUsecase;
 import clap.server.application.port.outbound.auth.otp.CommandOtpPort;
 import clap.server.application.port.outbound.auth.otp.LoadOtpPort;
 import clap.server.application.port.outbound.email.SendEmailPort;
+import clap.server.application.port.outbound.member.LoadMemberPort;
 import clap.server.common.annotation.architecture.ApplicationService;
 import clap.server.common.utils.VerificationCodeGenerator;
 import clap.server.domain.model.auth.Otp;
 import clap.server.domain.model.member.Member;
 import clap.server.exception.ApplicationException;
 import clap.server.exception.code.AuthErrorCode;
+import clap.server.exception.code.MemberErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
 @ApplicationService
 @RequiredArgsConstructor
-public class EmailVerificationService implements SendVerificationEmailUsecase, VerifyEmailCodeUsecase {
+ public class EmailVerificationService implements SendVerificationEmailUsecase, VerifyEmailCodeUsecase {
     private final MemberService memberService;
+    private final LoadMemberPort loadMemberPort;
     private final SendEmailPort sendEmailPort;
     private final CommandOtpPort commandOtpPort;
     private final LoadOtpPort loadOtpPort;
 
     @Override
     @Transactional
-    public void sendVerificationCode(Long memberId) {
-        Member member = memberService.findActiveMember(memberId);
+    public void sendVerificationCode(SendVerificationCodeRequest request) {
+        Member member = loadMemberPort.findByNicknameOrEmail(request.nickname(), request.email())
+                .orElseThrow(() -> new ApplicationException(MemberErrorCode.MEMBER_NOT_FOUND));
         String verificationCode = VerificationCodeGenerator.generateRandomCode();
         commandOtpPort.save(new Otp(member.getMemberInfo().getEmail(), verificationCode));
         sendEmailPort.sendVerificationEmail(member.getMemberInfo().getEmail(), member.getNickname(), verificationCode);
