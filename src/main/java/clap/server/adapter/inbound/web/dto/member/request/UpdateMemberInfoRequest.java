60dc799df9f468e2120e76a5ba2110166b335fc4
joowojr <joowojr@gmail.com>
2025-02-07 21:08:29 +0900

    CLAP-331 Feat: 프로필 이미지 삭제 로직 추가
    <footer>
    - 관련: #420

--- src/main/java/clap/server/adapter/inbound/web/dto/member/request/UpdateMemberInfoRequest.java
+++ src/main/java/clap/server/adapter/inbound/web/dto/member/request/UpdateMemberInfoRequest.java
@@ -2,10 +2,13 @@ package clap.server.adapter.inbound.web.dto.member.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record UpdateMemberInfoRequest(
         @NotBlank @Schema(description = "이름")
         String name,
+        @NotNull @Schema(description = "이미지 수정이 있을 시에는 false을, 이미지를 삭제할 때에는 true을 보냅니다.")
+        Boolean isProfileImageDeleted,
         @Schema(description = "아지트 알림 수신 여부")
         boolean agitNotification,
         @Schema(description = "이메일 알림 수신 여부")
