3745399741352e052bcdf0472a92662bee27fa0a
joowojr <joowojr@gmail.com>
2025-02-08 23:14:25 +0900

    CLAP-335 Fix: 잘못된 메서드 위치 수정

--- src/main/java/clap/server/application/service/task/FindManagersService.java
+++ src/main/java/clap/server/application/service/task/FindManagersService.java
@@ -1,8 +1,10 @@
 package clap.server.application.service.task;
 
 import clap.server.adapter.inbound.web.dto.task.response.FindManagersResponse;
-import clap.server.application.port.inbound.task.FindManagersUsecase;
+import clap.server.adapter.outbound.persistense.entity.task.constant.TaskStatus;
 import clap.server.application.port.inbound.domain.MemberService;
+import clap.server.application.port.inbound.task.FindManagersUsecase;
+import clap.server.application.port.outbound.task.LoadTaskPort;
 import clap.server.common.annotation.architecture.ApplicationService;
 import clap.server.domain.model.member.Member;
 import jakarta.transaction.Transactional;
@@ -17,14 +19,16 @@ import static clap.server.application.mapper.response.TaskResponseMapper.toFindM
 public class FindManagersService implements FindManagersUsecase {
 
     private final MemberService memberService;
+    private final LoadTaskPort loadTaskPort;
 
     @Transactional
     @Override
     public List<FindManagersResponse> findManagers() {
+        List<TaskStatus> targetStatuses = List.of(TaskStatus.IN_PROGRESS, TaskStatus.IN_REVIEWING);
         List<Member> managers = memberService.findActiveManagers();
         return managers.stream()
                 .map(manager -> {
-                    int remainingTasks = memberService.getRemainingTasks(manager.getMemberId());
+                    int remainingTasks = loadTaskPort.findTasksByMemberIdAndStatus(manager.getMemberId(), targetStatuses).size();
                     return toFindManagersResponse(manager, remainingTasks);
                 }).toList();
     }
