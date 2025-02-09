70dc5e389e6c0b28701b0db34ff05626a1830036
joowojr <joowojr@gmail.com>
2025-02-09 16:56:41 +0900

    CLAP-340 fix :작업 상태 변경 가능 상태 수정
    <footer>
    - #437

--- src/main/java/clap/server/application/port/inbound/task/UpdateTaskStatusUsecase.java
+++ src/main/java/clap/server/application/port/inbound/task/UpdateTaskStatusUsecase.java
@@ -1,6 +1,5 @@
 package clap.server.application.port.inbound.task;
 
-import clap.server.adapter.inbound.web.dto.task.response.UpdateTaskResponse;
 import clap.server.adapter.outbound.persistense.entity.task.constant.TaskStatus;
 
 public interface UpdateTaskStatusUsecase {
