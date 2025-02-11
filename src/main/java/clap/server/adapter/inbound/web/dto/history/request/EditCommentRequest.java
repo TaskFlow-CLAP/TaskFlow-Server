cf951aa6a39e640d072287879a2cdec84916421a
joowojr <joowojr@gmail.com>
2025-02-11 11:04:37 +0900

    Refactor: 사용하지 않는 dto deprecated 처리

--- src/main/java/clap/server/adapter/inbound/web/dto/history/request/EditCommentRequest.java
+++ src/main/java/clap/server/adapter/inbound/web/dto/history/request/EditCommentRequest.java
@@ -3,6 +3,7 @@ package clap.server.adapter.inbound.web.dto.history.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
+@Deprecated
 public record EditCommentRequest(
         @Schema(description = "댓글 내용")
         @NotBlank
