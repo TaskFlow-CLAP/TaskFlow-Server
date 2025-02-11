f4b5a26cf2f7560b43fb07f9477933c9d5017977
joowojr <joowojr@gmail.com>
2025-02-11 10:02:02 +0900

    CLAP-369 Refactor: 리팩토링으로 인한 테스트 코드 수정 사항 반영
    <footer>
    - #472

--- src/test/java/clap/server/application/service/task/UpdateTaskBoardServiceTest.java
+++ src/test/java/clap/server/application/service/task/UpdateTaskBoardServiceTest.java
@@ -1,15 +1,14 @@
 package clap.server.application.service.task;
 
 import clap.server.adapter.inbound.web.dto.task.request.UpdateTaskOrderRequest;
+import clap.server.adapter.outbound.persistense.entity.task.constant.TaskStatus;
 import clap.server.application.port.inbound.domain.MemberService;
 import clap.server.application.port.inbound.domain.TaskService;
 import clap.server.application.port.outbound.task.LoadTaskPort;
-import clap.server.application.service.webhook.SendNotificationService;
 import clap.server.domain.model.member.Member;
 import clap.server.domain.model.task.Task;
 import clap.server.domain.policy.task.ProcessorValidationPolicy;
 import clap.server.domain.policy.task.TaskOrderCalculationPolicy;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,11 +19,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 @ExtendWith(MockitoExtension.class)
 class UpdateTaskBoardServiceTest {
 
@@ -46,100 +44,97 @@ class UpdateTaskBoardServiceTest {
     @Mock
     private ProcessorValidationPolicy processorValidationPolicy;
 
-    @Mock
-    private SendNotificationService sendNotificationService;
-
-    @BeforeEach
-    void setUp() {
-        lenient().when(loadTaskPort.findByIdAndStatus(anyLong(), any()))
-                .thenReturn(Optional.empty());
-        lenient().when(taskOrderCalculationPolicy.calculateOrderForTop(any(), any()))
-                .thenReturn(100L);
-        lenient().when(taskOrderCalculationPolicy.calculateOrderForBottom(any(), any()))
-                .thenReturn(200L);
-        lenient().when(taskOrderCalculationPolicy.calculateNewProcessorOrder(anyLong(), anyLong()))
-                .thenReturn(150L);
-    }
-
-    @Test
-    @DisplayName("작업 순서를 업데이트-중간 이동")
-    void updateTaskOrder() {
-        // given
-        Long processorId = 1L;
-        UpdateTaskOrderRequest request = new UpdateTaskOrderRequest(2L, 0L, 3L); // prevTaskId = 0 (맨 위로 이동)
-
-        Member processor = mock(Member.class);
-        Task targetTask = mock(Task.class);
-        Task nextTask = mock(Task.class);
-
-        when(taskService.findById(request.targetTaskId())).thenReturn(targetTask);
-        when(memberService.findActiveMember(processorId)).thenReturn(processor);
-        when(loadTaskPort.findByIdAndStatus(anyLong(), any())).thenReturn(Optional.of(nextTask));
-
-        // when
-        updateTaskBoardService.updateTaskOrder(processorId, request);
-
-        // then
-        ArgumentCaptor<Long> orderCaptor = ArgumentCaptor.forClass(Long.class);
-        verify(targetTask).updateProcessorOrder(orderCaptor.capture());
-        assertEquals(150l, orderCaptor.getValue());
-
-        verify(taskService).upsert(targetTask);
-    }
-
     @Test
     @DisplayName("작업 순서를 업데이트 - 가장 상위로 이동")
     void updateTaskOrder_moveToTop() {
         // given
         Long processorId = 1L;
         UpdateTaskOrderRequest request = new UpdateTaskOrderRequest(0L, 2L, 1L); // prevTaskId = 0 (맨 위로 이동)
+        TaskStatus taskStatus = TaskStatus.IN_PROGRESS;
 
         Member processor = mock(Member.class);
         Task targetTask = mock(Task.class);
         Task nextTask = mock(Task.class);
+        Task prevTask = mock(Task.class);
 
         when(memberService.findActiveMember(processorId)).thenReturn(processor);
         when(taskService.findById(request.targetTaskId())).thenReturn(targetTask);
-        when(loadTaskPort.findByIdAndStatus(request.nextTaskId(), targetTask.getTaskStatus())).thenReturn(Optional.of(nextTask));
-
-        // Mock nextTask의 processorOrder 값을 반환
-        when(nextTask.getProcessorOrder()).thenReturn(200L);
-
-        when(taskOrderCalculationPolicy.calculateOrderForTop(null, nextTask)).thenReturn(100L);
+        when(targetTask.getTaskStatus()).thenReturn(taskStatus);
+        when(taskService.findByIdAndStatus(request.nextTaskId(), taskStatus)).thenReturn(nextTask);
+        when(loadTaskPort.findPrevOrderTaskByProcessorOrderAndStatus(processorId, taskStatus, nextTask.getProcessorOrder()))
+                .thenReturn(Optional.of(prevTask));
+        when(taskOrderCalculationPolicy.calculateOrderForTop(prevTask, nextTask)).thenReturn(100L);
 
         // when
         updateTaskBoardService.updateTaskOrder(processorId, request);
 
         // then
-        ArgumentCaptor<Long> orderCaptor = ArgumentCaptor.forClass(Long.class);
-        verify(targetTask).updateProcessorOrder(orderCaptor.capture());
-        assertEquals(100L, orderCaptor.getValue()); // 계산된 순서 값이 올바른지 확인
-
-        verify(taskService).upsert(targetTask); // 작업이 저장되었는지 확인
+        verify(targetTask).updateProcessorOrder(100L);
+        verify(taskService).upsert(targetTask);
+        verify(processorValidationPolicy).validateProcessor(processorId, targetTask);
     }
-
-
 
     @Test
     @DisplayName("작업 순서를 업데이트 - 가장 하위로 이동")
     void updateTaskOrder_moveToBottom() {
         // given
         Long processorId = 1L;
-        UpdateTaskOrderRequest request = new UpdateTaskOrderRequest(2L, 3L, 0L); // nextTaskId = 0 (맨 아래로 이동)
+        UpdateTaskOrderRequest request = new UpdateTaskOrderRequest(1L, 2L, 0L); // nextTaskId = 0 (맨 아래로 이동)
+        TaskStatus taskStatus = TaskStatus.IN_PROGRESS;
 
         Member processor = mock(Member.class);
         Task targetTask = mock(Task.class);
         Task prevTask = mock(Task.class);
+        Task nextTask = mock(Task.class);
 
-        doReturn(processor).when(memberService).findActiveMember(processorId);
-        doReturn(targetTask).when(taskService).findById(request.targetTaskId());
-        doReturn(Optional.of(prevTask)).when(loadTaskPort).findByIdAndStatus(anyLong(), any());
+        when(memberService.findActiveMember(processorId)).thenReturn(processor);
+        when(taskService.findById(request.targetTaskId())).thenReturn(targetTask);
+        when(targetTask.getTaskStatus()).thenReturn(taskStatus);
+        when(taskService.findByIdAndStatus(request.prevTaskId(), taskStatus)).thenReturn(prevTask);
+        when(loadTaskPort.findNextOrderTaskByProcessorOrderAndStatus(processorId, taskStatus, prevTask.getProcessorOrder()))
+                .thenReturn(Optional.of(nextTask));
+        when(taskOrderCalculationPolicy.calculateOrderForBottom(prevTask, nextTask)).thenReturn(200L);
 
         // when
         updateTaskBoardService.updateTaskOrder(processorId, request);
 
         // then
-        verify(taskService).upsert(targetTask);
         verify(targetTask).updateProcessorOrder(200L);
+        verify(taskService).upsert(targetTask);
+        verify(processorValidationPolicy).validateProcessor(processorId, targetTask);
+    }
+
+
+
+    @Test
+    @DisplayName("작업 순서를 업데이트 - 중간으로 이동")
+    void updateTaskOrder_moveToMiddle() {
+        // given
+        Long processorId = 1L;
+        UpdateTaskOrderRequest request = new UpdateTaskOrderRequest(2L, 3L, 1L); // prev와 next 둘 다 존재 (중간 이동)
+        TaskStatus taskStatus = TaskStatus.IN_PROGRESS;
+
+        Member processor = mock(Member.class);
+        Task targetTask = mock(Task.class);
+        Task prevTask = mock(Task.class);
+        Task nextTask = mock(Task.class);
+
+        when(memberService.findActiveMember(processorId)).thenReturn(processor);
+        when(taskService.findById(request.targetTaskId())).thenReturn(targetTask);
+        when(targetTask.getTaskStatus()).thenReturn(taskStatus);
+        when(taskService.findByIdAndStatus(request.prevTaskId(), taskStatus)).thenReturn(prevTask);
+        when(taskService.findByIdAndStatus(request.nextTaskId(), taskStatus)).thenReturn(nextTask);
+        when(prevTask.getProcessorOrder()).thenReturn(100L);
+        when(nextTask.getProcessorOrder()).thenReturn(200L);
+        when(taskOrderCalculationPolicy.calculateNewProcessorOrder(prevTask.getProcessorOrder(), nextTask.getProcessorOrder()))
+                .thenReturn(150L);
+
+        // when
+        updateTaskBoardService.updateTaskOrder(processorId, request);
+
+        // then
+        verify(targetTask).updateProcessorOrder(150L);
+        verify(taskService).upsert(targetTask);
+        verify(processorValidationPolicy).validateProcessor(processorId, targetTask);
     }
 }
