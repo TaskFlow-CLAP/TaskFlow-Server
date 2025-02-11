0668fa00f6c5a8fae92a83055a0e5f411c38829f
joowojr <joowojr@gmail.com>
2025-02-11 15:36:27 +0900

    CLAP-379 Refactor: 비밀번호 정규식 정책화
    <footer>
    - #484

--- src/main/java/clap/server/domain/policy/member/PasswordPolicy.java
+++ src/main/java/clap/server/domain/policy/member/PasswordPolicy.java
@@ -1,11 +1,14 @@
-package clap.server.common.annotation.validation.password;
+package clap.server.domain.policy.member;
 
+import clap.server.common.annotation.architecture.Policy;
+import clap.server.common.annotation.validation.password.ValidPassword;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 import java.util.regex.Pattern;
 
-public class PasswordValidator implements ConstraintValidator<ValidPassword, String> {
+@Policy
+public class PasswordPolicy implements ConstraintValidator<ValidPassword, String> {
 
     private static final String PASSWORD_REGEX = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()_+{}\\[\\]:;<>,.?/~`-]).{8,20}$";
 
