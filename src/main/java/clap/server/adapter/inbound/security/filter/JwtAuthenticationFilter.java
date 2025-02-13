dbc602907528d7030ad93b1ef4872522e35980c4
joowojr <joowojr@gmail.com>
2025-02-13 16:30:17 +0900

    CLAP-425 Fix: 비밀번호 재설정 경로에도 잠금 계정 여부를 확인하도록 수정
    <footer>
    - #554

--- src/main/java/clap/server/adapter/inbound/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/clap/server/adapter/inbound/security/filter/JwtAuthenticationFilter.java
@@ -49,8 +49,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             SWAGGER_ENDPOINTS
     ).flatMap(Arrays::stream).toArray(String[]::new);
 
-    public static final String[] ANONYMOUS_ENDPOINTS = {LOGIN_ENDPOINT, PASSWORD_EMAIL_ENDPOINT};
-
     @Override
     protected void doFilterInternal(
             @NotNull HttpServletRequest request,
