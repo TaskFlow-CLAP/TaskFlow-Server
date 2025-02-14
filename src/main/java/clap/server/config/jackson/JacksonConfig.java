e64365ba97010ab031a2e06945a2c43d2b7cb29d
joowojr <joowojr@gmail.com>
2025-02-14 09:53:18 +0900

    CLAP-429 Fix: LocalDateTime 직렬화 모듈 추가
    <footer>
    - #557

--- src/main/java/clap/server/config/jackson/JacksonConfig.java
+++ src/main/java/clap/server/config/jackson/JacksonConfig.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Safelist;
@@ -13,7 +15,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
 
-// XSS 방지를 위한 Jackson 설정
+
 @Slf4j
 @Configuration
 public class JacksonConfig {
@@ -21,12 +23,19 @@ public class JacksonConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
-        SimpleModule module = new SimpleModule();
-        module.addDeserializer(String.class, new JsonHtmlXssDeserializer());
-        mapper.registerModule(module);
+
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        // XSS 방지를 위한 커스텀 모듈 추가
+        SimpleModule xssModule = new SimpleModule();
+        xssModule.addDeserializer(String.class, new JsonHtmlXssDeserializer());
+        mapper.registerModule(xssModule);
+
         return mapper;
     }
 
+    // XSS 방지를 위한 Jackson 설정
     public static class JsonHtmlXssDeserializer extends JsonDeserializer<String> {
         @Override
         public String deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
