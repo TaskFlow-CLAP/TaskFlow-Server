b3a1468d7f5cf66f901ca85f618dd148bbd1c8da
joowojr <joowojr@gmail.com>
2025-02-18 23:00:31 +0900

    CLAP-453 Refactor: 지연 로딩이 설정되어 있는 member에 대해 추가적인 쿼리가 발생되지 않도록 매퍼 수정
    #592

--- src/main/java/clap/server/adapter/outbound/persistense/mapper/CommentPersistenceMapper.java
+++ src/main/java/clap/server/adapter/outbound/persistense/mapper/CommentPersistenceMapper.java
@@ -1,25 +1,34 @@
 package clap.server.adapter.outbound.persistense.mapper;
 
 import clap.server.adapter.outbound.persistense.entity.task.CommentEntity;
-
-import clap.server.adapter.outbound.persistense.mapper.common.PersistenceMapper;
-
+import clap.server.domain.model.member.Member;
 import clap.server.domain.model.task.Comment;
-
+import org.hibernate.Hibernate;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring", uses = {MemberPersistenceMapper.class, TaskPersistenceMapper.class})
-public interface CommentPersistenceMapper  extends PersistenceMapper<CommentEntity, Comment> {
+public abstract class CommentPersistenceMapper {
 
-    @Override
+    @Autowired
+    MemberPersistenceMapper memberPersistenceMapper;
+
     @Mapping(source = "modified", target = "isModified")
     @Mapping(source = "deleted", target = "isDeleted")
+    @Mapping(target = "member", expression = "java(mapMember(entity))")
     @Mapping(target = "task", ignore = true)
-    Comment toDomain(final CommentEntity entity);
+    public abstract Comment toDomain(final CommentEntity entity);
 
-    @Override
     @Mapping(source = "modified", target = "isModified")
     @Mapping(source = "deleted", target = "isDeleted")
-    CommentEntity toEntity(final Comment domain);
+    public abstract CommentEntity toEntity(final Comment domain);
+
+    protected Member mapMember(CommentEntity entity) {
+        if (entity == null || entity.getMember() == null || !Hibernate.isInitialized(entity.getMember())) {
+            return null;
+        }
+        return memberPersistenceMapper.toDomain(entity.getMember());
+    }
+
 }
